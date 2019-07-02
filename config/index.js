6a82d65af93e70183f5d5a04796fb17b748e2994
LINZJAI <969060748@qq.com>
2019-07-02 16:36:25 +0800

    打包

--- config/index.js
+++ config/index.js
@@ -48,7 +48,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, "../dist"),
     assetsSubDirectory: "static",
     // assetsPublicPath: './webpage/dist/',
-    assetsPublicPath: "/crNursing/webpage/dist/",
+    assetsPublicPath: "./",
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
