955606790665e695706521060347748842ba2e29
weijun-lai <weijun.lai@uqconnect.edu.au>
2019-07-15 11:25:33 +0800

    隐藏 威县 血糖 功能：页面血糖组件 根据医院名自动切换显示

--- src/Part/main.vue
+++ src/Part/main.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-    <HLheader></HLheader>
+    <!-- <HLheader></HLheader> -->
+    <component :is="switchCompt()" />
     <router-view></router-view>
   </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
 </style>
 <script>
-  import HLheader from '@/components/HLheader/HLheader'
-  export default {
-    data() {
-      return {
-        msg: 'hello vue'
-      }
-    },
-    components: {
-      HLheader
+import HLheader from "@/components/HLheader/HLheader"; // 东莞市厚街医院
+import WeiXianHeader from "@/components/HLheader/WeiXianHeader"; // 威县人民医院
+export default {
+  data() {
+    return {
+      msg: "hello vue"
+    };
+  },
+  created() {},
+  methods: {
+    // 依据医院名字，标题组件切换
+    switchCompt(HisName = process.env.HOSPITAL_NAME) {
+      let hisList = {
+        威县人民医院: "WeiXianHeader",
+        东莞市厚街医院: "HLheader"
+      };
+      return hisList[HisName] || "HLheader";
     }
+  },
+  components: {
+    HLheader,
+    WeiXianHeader
   }
+};
 </script>
