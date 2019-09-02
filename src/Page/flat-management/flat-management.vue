d545313da366c26dd1ce8d0c840e92b91236c39b
linagjinmei <liangjinmei@cr-health.com>
2019-09-02 09:33:27 +0800

    fix扁平管理bug

--- src/Page/flat-management/flat-management.vue
+++ src/Page/flat-management/flat-management.vue
@@ -143,8 +143,8 @@ export default {
         typeId: "", //类型id
         deptCode: "", //科室代码
         status: "", //状态1:提交未审核，2：已审核
-        checkDateStart: "", //检查日期开始日期（yyyy-MM-dd hh:mm:ss）
-        checkDateEnd: "", //检查日期结束日期（yyyy-MM-dd hh:mm:ss
+        checkDateStart: "", //检查日期开始日期（yyyy-MM-dd）
+        checkDateEnd: "", //检查日期结束日期（yyyy-MM-dd
         pageIndex: 1, //页码
         pageSize: 20, //每页条数
         pageSize: 20
@@ -252,12 +252,8 @@ export default {
     setTableData() {
       this.pageLoadng = true;
       this.query.deptCode = this.deptCode;
-      this.query.checkDateStart = this.query.checkDateStart
-        ? dayjs(this.query.checkDateStart).format("YYYY-MM-DD")
-        : dayjs(new Date()).format("YYYY-MM-DD");
-      this.query.checkDateEnd = this.query.checkDateEnd
-        ? dayjs(this.query.checkDateEnd).format("YYYY-MM-DD")
-        : dayjs(new Date()).format("YYYY-MM-DD");
+      this.query.checkDateStart = dayjs(this.query.checkDateStart).format("YYYY-MM-DD");
+      this.query.checkDateEnd = dayjs(this.query.checkDateEnd).format("YYYY-MM-DD");
       getList(this.query).then(
         res => {
           if (res.data && res.data.code == 200) {
