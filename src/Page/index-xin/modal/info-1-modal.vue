653da6339543d8c5801eae616dd100c0ad661c18
LINZJAI <969060748@qq.com>
2019-09-07 16:03:17 +0800

    患者详情页路由参数简化

--- src/Page/index-xin/modal/info-1-modal.vue
+++ src/Page/index-xin/modal/info-1-modal.vue
@@ -17,12 +17,16 @@
         <div class="list-con">
           <span class="key">来源：</span>
           <span class="value">{{data.taskSource}}</span>
-        </div> 
+        </div>
         <div flex="cross:center" class="icon-con">
-        <div class="table-name" v-for="item in (details.evalIcons &&  details.evalIcons.slice(0,6))" :key="item.formName" :title="item.description" :style="{background: item.iconTextRgb}">
-              {{item.iconText}}
-          </div>
-         </div>  
+          <div
+            class="table-name"
+            v-for="item in (details.evalIcons &&  details.evalIcons.slice(0,6))"
+            :key="item.formName"
+            :title="item.description"
+            :style="{background: item.iconTextRgb}"
+          >{{item.iconText}}</div>
+        </div>
       </div>
       <div slot="button">
         <el-button class="modal-btn" @click="close">取消</el-button>
@@ -32,70 +36,86 @@
   </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
-.list-con
-  margin 0 40px 10px
+.list-con {
+  margin: 0 40px 10px;
   font-size: 14px;
-  .key
+
+  .key {
     color: #687179;
-  .value
-    color: #333333;  
-.icon-con
-  margin 0 38px 10px  
-  min-height 20px  
-.table-name
+  }
+
+  .value {
+    color: #333333;
+  }
+}
+
+.icon-con {
+  margin: 0 38px 10px;
+  min-height: 20px;
+}
+
+.table-name {
   background: #FF263C;
   border-radius: 2px;
-  width 18px
-  height 18px;
-  margin-left 4px
+  width: 18px;
+  height: 18px;
+  margin-left: 4px;
   font-size: 12px;
   color: #FFFFFF;
-  text-align: center
-  line-height 18px   
-.modal-btn
-  overflow hidden   
+  text-align: center;
+  line-height: 18px;
+}
+
+.modal-btn {
+  overflow: hidden;
+}
 </style>
 <script>
-import common from '@/common/mixin/common.mixin.js'
-import mixin from '../mixins/index.js'
-import {patList} from '../api/index-xin.js'
-import qs from 'qs'
-  export default {
-    mixins: [common, mixin],
-    data() {
-      return {
-        data: {},
-        details: {},
-        iconLoading: false,
-      }
+import common from "@/common/mixin/common.mixin.js";
+import mixin from "../mixins/index.js";
+import { patList } from "../api/index-xin.js";
+import qs from "qs";
+export default {
+  mixins: [common, mixin],
+  data() {
+    return {
+      data: {},
+      details: {},
+      iconLoading: false
+    };
+  },
+  methods: {
+    open(data) {
+      this.data = data;
+      this.details = {};
+      this.iconLoading = true;
+      patList(this.deptCode, data.patientId, data.visitId).then(res => {
+        this.details = res.data.data;
+        this.iconLoading = false;
+      });
+      this.$refs.modal.open();
     },
-    methods: {
-      open(data) {
-        this.data = data
-        this.details = {} 
-        this.iconLoading = true
-        patList(this.deptCode, data.patientId, data.visitId).then(res => {
-          this.details = res.data.data
-          this.iconLoading = false
-        })
-        this.$refs.modal.open()
-      },
-      close() {
-        this.$refs.modal.close()
-      },
-      post() {
-        let obj = {}
-        let item = this.details
-        for(let i in item) {
-          if(item[i]) {
-            obj[i] = item[i]
-          }
+    close() {
+      this.$refs.modal.close();
+    },
+    post() {
+      let obj = {};
+      let item = this.details;
+      for (let i in item) {
+        if (item[i]) {
+          obj[i] = item[i];
         }
-        window.open(`/crNursing/home?${qs.stringify(obj)}`)
-        this.close()
       }
-    },
-    components: {}
-  }
+      window.open(
+        `/crNursing/home?${qs.stringify({
+          patientId: obj.patientId,
+          visitId: obj.visitId
+        })}`
+      );
+      this.close();
+    }
+  },
+  components: {}
+};
 </script>
 
