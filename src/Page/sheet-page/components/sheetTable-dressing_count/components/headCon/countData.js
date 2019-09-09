2a7df5c09fd1bcc8071b2deb0a347c8cfd48d40b
linagjinmei <liangjinmei@cr-health.com>
2019-09-09 10:02:17 +0800

    产科记录单bug

---
+++ src/Page/sheet-page/components/sheetTable-dressing_count/components/headCon/countData.js
@@ -0,0 +1,146 @@
+export const firstTableHead = [
+  "名称",
+  "接生前",
+  "接生后",
+  "名称",
+  "接生前",
+  "接生后",
+]
+
+export const firstTableBody = [{
+  name: "血管钳",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "纱块",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+},{
+  name: "持针器",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "有尾纱",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+},{
+  name: "剪刀",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "脐卷",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+},{
+  name: "镊子",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "注射器",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+},{
+  name: "",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "缝针",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+},{
+  name: "",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "封闭针",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+},{
+  name: "",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+},{
+  name: "",
+  deliveryBefore: "",
+  deliveryAfter: "",
+  name1: "",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+}]
+
+export const firstTableBody1 = {
+  name1: "血管钳",
+  deliveryBefore1: "",
+  deliveryAfter1: ""
+,
+  name2: "纱块",
+  deliveryBefore2: "",
+  deliveryAfter2: ""
+,
+  name3: "持针器",
+  deliveryBefore3: "",
+  deliveryAfter3: ""
+,
+  name4: "有尾纱",
+  deliveryBefore4: "",
+  deliveryAfter4: ""
+,
+  name5: "剪刀",
+  deliveryBefore5: "",
+  deliveryAfter5: ""
+,
+  name6: "脐带",
+  deliveryBefore6: "",
+  deliveryAfter6: ""
+,
+  name7: "镊子",
+  deliveryBefore7: "",
+  deliveryAfter7: ""
+,
+  name8: "注射器",
+  deliveryBefore8: "",
+  deliveryAfter8: ""
+,
+  name9: "",
+  deliveryBefore9: "",
+  deliveryAfter9: ""
+,
+  name10: "缝针",
+  deliveryBefore10: "",
+  deliveryAfter10: ""
+,
+  name11: "",
+  deliveryBefore11: "",
+  deliveryAfter11: ""
+,
+  name12: "封闭针",
+  deliveryBefore12: "",
+  deliveryAfter12: ""
+,
+  name13: "",
+  deliveryBefore13: "",
+  deliveryAfter13: ""
+,
+  name14: "",
+  deliveryBefore14: "",
+  deliveryAfter14: ""
+,
+  name15: "",
+  deliveryBefore15: "",
+  deliveryAfter15: ""
+,
+  name16: "",
+  deliveryBefore16: "",
+  deliveryAfter16: ""
+}
+var cacheItem = {}
+
+// export const newTabBody = firstTableBody1.map(function(item, index) {
+//   let trimData = []
+//   if (index % 2 == 0) {
+//     cacheItem = item
+//   } else {
+//     let cacheItem = Object.assign(item,cacheItem)
+//     trimData.push(cacheItem)
+//   }
+//   return trimData
+// })
+
+// export trimData
