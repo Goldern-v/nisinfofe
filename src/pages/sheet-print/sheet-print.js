0243c5322dd7b954c882ca6695a6c683f45c85bf
LINZJAI <969060748@qq.com>
2019-09-18 17:12:25 +0800

    修改

--- src/pages/sheet-print/sheet-print.js
+++ src/pages/sheet-print/sheet-print.js
@@ -1,11 +1,15 @@
-import Vue from 'Vue'
-import sheetPage from './components/sheet-page'
-import '../../assets/css/flex.css'
-import '@/assets/css/tinyreset.css'
-import '@/assets/css/style.styl'
-import './style/print.styl'
+import Vue from "Vue";
+import sheetPage from "./components/sheet-page";
+import "../../assets/css/flex.css";
+import "@/assets/css/tinyreset.css";
+import "@/assets/css/style.styl";
+import "./style/print.styl";
+
+Vue.prototype.HOSPITAL_ID = process.env.HOSPITAL_ID;
+Vue.prototype.HOSPITAL_NAME = process.env.HOSPITAL_NAME;
+Vue.prototype.HOSPITAL_NAME_SPACE = process.env.HOSPITAL_NAME_SPACE;
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(sheetPage)
-})
+});
