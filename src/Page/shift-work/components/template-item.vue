68e8fffff70c9780b95bcecf666b204b1394e5f0
LINZJAI <969060748@qq.com>
2020-01-13 17:44:26 +0800

    交班志图标放大

--- src/Page/shift-work/components/template-item.vue
+++ src/Page/shift-work/components/template-item.vue
@@ -14,70 +14,79 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      data: Object,
+export default {
+  props: {
+    data: Object
+  },
+  data: () => ({}),
+  methods: {
+    onClick() {
+      this.$emit("click", this.data);
     },
-    data: () => ({}),
-    methods: {
-      onClick () {
-        this.$emit('click', this.data)
-      },
-      onEdit () {
-        this.$emit('edit', this.data)
-      },
-      onRemove () {
-        this.$emit('remove', this.data)
-      }
+    onEdit() {
+      this.$emit("edit", this.data);
+    },
+    onRemove() {
+      this.$emit("remove", this.data);
     }
   }
+};
 </script>
 
 <style lang="stylus" scoped>
-  .template-item
-    position relative
-    padding 10px 20px
-    user-select none
+.template-item {
+  position: relative;
+  padding: 10px 20px;
+  user-select: none;
 
-    &::after
-      position absolute
-      left 20px
-      right 20px
-      bottom 0
-      content ""
-      display block
-      content ""
-      background #ECEEEF
-      height 1px
+  &::after {
+    position: absolute;
+    left: 20px;
+    right: 20px;
+    bottom: 0;
+    content: '';
+    display: block;
+    content: '';
+    background: #ECEEEF;
+    height: 1px;
+  }
 
-    &:hover
-      background: #f4f2f5;
-      cursor pointer
-      transition all .3s
+  &:hover {
+    background: #f4f2f5;
+    cursor: pointer;
+    transition: all 0.3s;
 
-      .actions
-        display block
+    .actions {
+      display: block;
+    }
+  }
 
-    .title
-      font-size 14px
-      color #333333
-      font-weight bold
-      min-height 0px
-      margin-bottom 10px
-      line-height 1.4em
+  .title {
+    font-size: 14px;
+    color: #333333;
+    font-weight: bold;
+    min-height: 0px;
+    margin-bottom: 10px;
+    line-height: 1.4em;
+  }
 
-    .content
-      font-size 13px
-      color #687179
-      line-height 1.4em
+  .content {
+    font-size: 13px;
+    color: #687179;
+    line-height: 1.4em;
+  }
 
-    .actions
-      position absolute
-      bottom 5px
-      right 5px
-      display none
-      i
-        color #687179
-        font-size 16px
-        padding 4px 2px
+  .actions {
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+    display: none;
+
+    i {
+      color: #687179;
+      font-size: 18px;
+      padding: 4px 2px;
+    }
+  }
+}
 </style>
