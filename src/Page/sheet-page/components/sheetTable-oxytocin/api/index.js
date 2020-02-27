d03b3caf281d7b8a12b3f375eceeb81562cc9192
liangjinmei <l>
2020-02-27 14:32:27 +0800

    催产素记录单孕/产提取入院评估单

--- src/Page/sheet-page/components/sheetTable-oxytocin/api/index.js
+++ src/Page/sheet-page/components/sheetTable-oxytocin/api/index.js
@@ -1,13 +1,9 @@
 import axios from "@/api/axios";
-import { apiPath } from "@/api/apiConfig";
-import sheetInfo from "../components/config/sheetInfo";
-import qs from "qs";
+import {
+  apiPath
+} from "@/api/apiConfig";
 
-// 分类合计
-export const putGroupCount = (patientId, visitId, startTime, endTime) => {
-  return axios.post(`${apiPath}record/${sheetInfo.sheetType}/putGroupCount`, {
-    startTime,
-    endTime,
-    blockId: sheetInfo.selectBlock.id
-  });
+// 获取最新一次评估单信息
+export const getLastDetail = data => {
+  return axios.post(`${apiPath}form/getLastDetail`, data);
 };
