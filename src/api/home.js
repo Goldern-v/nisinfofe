24bf0d17bfd000183b5b580a78a7fd69deff1cfe
liangjinmei <liangjinmei@cr-health.com>
2020-06-19 17:02:42 +0800

    病人详情首页护理评估接口调整

--- src/api/home.js
+++ src/api/home.js
@@ -7,7 +7,7 @@ export const getNote = (patientId, visitId, HisName = process.env.HOSPITAL_NAME)
     let hisList = {
         威县人民医院: `eval/nursingWithNewForm/${patientId}/${visitId}/all`,
         德州市陵城区人民医院: `eval/nursingWithNewForm/${patientId}/${visitId}/all`,
-        东莞市厚街医院: `eval/nursing/${patientId}/${visitId}/all`
+        东莞市厚街医院: `eval/nursingWithNewForm/${patientId}/${visitId}/all`
     };
     let url = hisList[HisName] || `eval/nursing/${patientId}/${visitId}/all`
     return axios.get(`${apiPath}${url}`)
