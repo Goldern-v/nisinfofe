f8b66b53e257180f08e5296d412e89863b807536
liangjinmei <liangjinmei@cr-health.com>
2020-06-30 16:29:56 +0800

    陵城记录单修改

--- src/Page/sheet-page/components/config/picu_lc/th.js
+++ src/Page/sheet-page/components/config/picu_lc/th.js
@@ -2,295 +2,286 @@ export default {
   // 科室的名称
   deptType: '',
   style: {
-    width: "1520px"
+    width: "1420px"
   },
   th: {
     top: [{
-        name: '日期',
-        style: {
-          minWidth: '36px',
-          maxWidth: '36px',
-        },
-        colspan: '1',
-        rowspan: '3'
+      name: '日期',
+      style: {
+        minWidth: '36px',
+        maxWidth: '36px',
       },
-      {
-        name: '时间',
-        style: {
-          minWidth: '36px',
-          maxWidth: '36px',
-        },
-        colspan: '1',
-        rowspan: '3'
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: '时间',
+      style: {
+        minWidth: '36px',
+        maxWidth: '36px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         生命体征项目
         `,
-        style: {},
-        colspan: '6',
-        rowspan: '1',
-      },
-      {
-        name: `
+      style: {},
+      colspan: '6',
+      rowspan: '1',
+    },
+    {
+      name: `
         出量
         `,
-        colspan: "5",
-        rowspan: "1",
-        canSet: false
-      },
-      {
-        name: `
+      colspan: "5",
+      rowspan: "1",
+      canSet: false
+    },
+    {
+      name: `
         入量
         `,
-        colspan: "4",
-        rowspan: "1",
-        canSet: false
-      },
-      {
-        name: `
+      colspan: "4",
+      rowspan: "1",
+      canSet: false
+    },
+    {
+      name: `
         基础护理
         `,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '5',
-        rowspan: '1',
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
       },
-      {
-        name: `
+      colspan: '5',
+      rowspan: '1',
+    },
+    {
+      name: `
         特殊情况记录
         `,
-        style: {
-          minWidth: '306px',
-          maxWidth: '306px',
-        },
-        colspan: '1',
-        rowspan: '3'
+      style: {
+        minWidth: '295px',
+        maxWidth: '295px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         护士签名
         `,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '3'
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         审核签名
         `,
-        style: {},
-        colspan: '1',
-        rowspan: '3'
-      }
+      style: {},
+      colspan: '1',
+      rowspan: '3'
+    }
     ],
     mid: [{
-        name: `
+      name: `
         体温℃
         `,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2',
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '2',
+    },
+    {
+      name: `
         脉搏<br/>次/分
         `,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '2',
-        style: {
-          whiteSpace: 'nowrap',
-          minWidth: '55px',
-          maxWidth: '55px',
-        }
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `
         呼吸<br/>次/分
         `,
-        style: {
-          minWidth: '60px',
-          maxWidth: '60px',
-        },
-        colspan: '1',
-        rowspan: '2',
-        style: {
-          whiteSpace: 'nowrap',
-          minWidth: '38px',
-        }
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `
         血压mmHg
         `,
-        style: {
-          minWidth: '60px',
-          maxWidth: '60px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `
         氧饱和度%
         `,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px'
-        },
-        colspan: '1',
-        rowspan: '2'
+      style: {
+        minWidth: '45px',
+        maxWidth: '45px'
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `
         神志
         `,
-        style: {
-          minWidth: '36px',
-          maxWidth: '36px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      style: {
+        minWidth: '36px',
+        maxWidth: '36px',
       },
+      colspan: '1',
+      rowspan: '2'
+    },
 
-      {
-        name: `尿量ml`,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2'
+    {
+      name: `尿量ml`,
+      style: {
+        minWidth: '45px',
+        maxWidth: '45px',
       },
-      {
-        name: `呕吐ml`,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `呕吐ml`,
+      style: {
+        minWidth: '45px',
+        maxWidth: '45px',
       },
-      {
-        name: `大便ml`,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `大便ml`,
+      style: {
+        minWidth: '45px',
+        maxWidth: '45px',
       },
-      {
-        name: `其他引流量ml`,
-        style: {
-          minWidth: '80px',
-          maxWidth: '80px'
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `其他引流量ml`,
+      style: {
+        minWidth: '50px',
+        maxWidth: '50px'
       },
-      {
-        name: `其他`,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `其他`,
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `口服ml`,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `口服ml`,
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `静脉ml`,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `静脉ml`,
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `肌肉注释ml`,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `肌肉注释ml`,
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `灌肠ml`,
-        style: {
-          minWidth: '45px',
-          maxWidth: '45px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `灌肠ml`,
+      style: {
+        minWidth: '40px',
+        maxWidth: '40px',
       },
-      {
-        name: `皮肤情况`,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `皮肤情况`,
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
       },
-      {
-        name: `管道护理`,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `管道护理`,
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
       },
-      {
-        name: `气道护理`,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `气道护理`,
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
       },
-      {
-        name: `痰液性状`,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '2'
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `痰液性状`,
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
       },
-      {
-        name: `其他`,
-        style: {
-          minWidth: '55px',
-          maxWidth: '55px',
-        },
-        colspan: '1',
-        rowspan: '2'
-      }
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `其他`,
+      style: {
+        minWidth: '55px',
+        maxWidth: '55px',
+      },
+      colspan: '1',
+      rowspan: '2'
+    }
     ],
     bottom: []
   }
