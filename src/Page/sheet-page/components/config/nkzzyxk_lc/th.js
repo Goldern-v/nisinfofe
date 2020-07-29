705c2b74ed778113f208cd0fe937466c59a9779b
liangjinmei <liangjinmei@cr-health.com>
2020-07-29 11:12:15 +0800

    陵城记录单修改

--- src/Page/sheet-page/components/config/nkzzyxk_lc/th.js
+++ src/Page/sheet-page/components/config/nkzzyxk_lc/th.js
@@ -1,37 +1,38 @@
 export default {
   // 科室的名称
-  deptType: '',
+  deptType: "",
   style: {
     width: "1270px"
   },
   th: {
-    top: [{
-        name: '日期',
+    top: [
+      {
+        name: "日期",
         style: {
-          minWidth: '36px',
-          maxWidth: '36px',
+          minWidth: "36px",
+          maxWidth: "36px"
         },
-        colspan: '1',
-        rowspan: '3'
+        colspan: "1",
+        rowspan: "3"
       },
       {
-        name: '时间',
+        name: "时间",
         style: {
-          minWidth: '36px',
-          maxWidth: '36px',
+          minWidth: "36px",
+          maxWidth: "36px"
         },
-        colspan: '1',
-        rowspan: '3'
+        colspan: "1",
+        rowspan: "3"
       },
       //神志
       {
-        name: '神志',
+        name: "神志",
         style: {
-          minWidth: '36px',
-          maxWidth: '36px',
+          minWidth: "36px",
+          maxWidth: "36px"
         },
-        colspan: '1',
-        rowspan: '3'
+        colspan: "1",
+        rowspan: "3"
       },
       //体温
       {
@@ -39,11 +40,11 @@ export default {
         体温<br/>(℃)
         `,
         style: {},
-        colspan: '1',
-        rowspan: '3',
+        colspan: "1",
+        rowspan: "3",
         style: {
-          whiteSpace: 'nowrap',
-          minWidth: '38px',
+          whiteSpace: "nowrap",
+          minWidth: "38px"
         }
       },
       //心率
@@ -52,11 +53,11 @@ export default {
         心率<br/>(次/分)
         `,
         style: {},
-        colspan: '1',
-        rowspan: '3',
+        colspan: "1",
+        rowspan: "3",
         style: {
-          whiteSpace: 'nowrap',
-          minWidth: '38px',
+          whiteSpace: "nowrap",
+          minWidth: "38px"
         }
       },
       //血压
@@ -65,12 +66,12 @@ export default {
         血压<br/>(mmHg)
         `,
         style: {
-          minWidth: '52px',
-          maxWidth: '52px',
-          whiteSpace: 'nowrap'
+          minWidth: "52px",
+          maxWidth: "52px",
+          whiteSpace: "nowrap"
         },
-        colspan: '1',
-        rowspan: '3'
+        colspan: "1",
+        rowspan: "3"
       },
       //呼吸
       {
@@ -78,42 +79,42 @@ export default {
         呼吸<br/>(次/分)
         `,
         style: {},
-        colspan: '1',
-        rowspan: '3',
+        colspan: "1",
+        rowspan: "3",
         style: {
-          whiteSpace: 'nowrap',
-          minWidth: '38px',
+          whiteSpace: "nowrap",
+          minWidth: "38px"
         }
       },
       //SPO2
       {
         name: `
-        SPO2
+        SPO2<br/>(%)
         `,
         style: {
-          minWidth: '40px',
-          maxWidth: '40px'
+          minWidth: "40px",
+          maxWidth: "40px"
         },
-        colspan: '1',
-        rowspan: '3'
+        colspan: "1",
+        rowspan: "3"
       },
       //入量
       {
         name: `
         入量
         `,
-        colspan: '2',
-        rowspan: '1',
-        canSet: false,
+        colspan: "2",
+        rowspan: "1",
+        canSet: false
       },
       //出量
       {
         name: `
         出量
         `,
-        colspan: '2',
-        rowspan: '1',
-        canSet: false,
+        colspan: "2",
+        rowspan: "1",
+        canSet: false
       },
       //护理记录
       {
@@ -121,11 +122,11 @@ export default {
         护理记录
         `,
         style: {
-          minWidth: '346px',
-          maxWidth: '346px',
+          minWidth: "346px",
+          maxWidth: "346px"
         },
-        colspan: '1',
-        rowspan: '3'
+        colspan: "1",
+        rowspan: "3"
       },
       //签名
       {
@@ -133,56 +134,57 @@ export default {
         护士签名
         `,
         style: {},
-        colspan: '1',
-        rowspan: '3'
+        colspan: "1",
+        rowspan: "3"
       }
     ],
-    mid: [{
+    mid: [
+      {
         name: `
         项目
         `,
         style: {
-          minWidth: '110px',
-          maxWidth: '110px'
+          minWidth: "110px",
+          maxWidth: "110px"
         },
-        colspan: '1',
-        rowspan: '2'
+        colspan: "1",
+        rowspan: "2"
       },
       {
         name: `
         入量<br/>(ml)
         `,
-        colspan: '1',
-        rowspan: '2',
+        colspan: "1",
+        rowspan: "2",
         style: {
-          minWidth: '66px',
-          maxWidth: '66px',
-        },
+          minWidth: "66px",
+          maxWidth: "66px"
+        }
       },
       {
         name: `
         项目
         `,
         style: {
-          minWidth: '110px',
-          maxWidth: '110px'
+          minWidth: "110px",
+          maxWidth: "110px"
         },
-        colspan: '1',
-        rowspan: '2'
+        colspan: "1",
+        rowspan: "2"
       },
       {
         name: `
         出量<br/>(ml)
         `,
-        colspan: '1',
-        rowspan: '2',
+        colspan: "1",
+        rowspan: "2",
         style: {
-          minWidth: '66px',
-          maxWidth: '66px',
-          whiteSpace: 'nowrap'
-        },
-      },
+          minWidth: "66px",
+          maxWidth: "66px",
+          whiteSpace: "nowrap"
+        }
+      }
     ],
     bottom: []
   }
-}
+};
