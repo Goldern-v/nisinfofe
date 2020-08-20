aa882a8a993325695df5d4f165f740c94fc1d5b5
liangjinmei <liangjinmei@cr-health.com>
2020-08-20 15:27:04 +0800

    陵城交班志打印

--- src/Page/shift-work-lc/print-formatter.js
+++ src/Page/shift-work-lc/print-formatter.js
@@ -11,46 +11,23 @@ export default function(win) {
   const tbody = table.children[2];
   const trs = tbody.querySelectorAll("tr");
 
-  let patients = Array.prototype.slice.call(trs, 0, -2); // 病人列表
-  let rest = Array.prototype.slice.call(trs, -2); // 特殊情况（两行）
+  let patients = Array.prototype.slice.call(trs, 0); // 病人列表
 
-  const emptyRow = document.createElement("tr");
-  emptyRow.innerHTML =
-    '<td style="border: 1px solid black;"></td>'.repeat(3) +
-    '<td colspan="3" style="border: 1px solid black;"></td>'.repeat(3);
-  emptyRow.style.height = "30px";
 
   // 移除空行
   patients = patients.filter(row => {
     return row.innerText.trim() !== "";
   });
 
-  if (patients.length === 0) {
-    patients.push(emptyRow.cloneNode(true));
-  }
-
   let page, newTable, newTableBody, row;
 
-  while ((row = patients.shift() || rest)) {
-    const h =
-      row === rest
-        ? row[0].offsetHeight + row[1].offsetHeight
-        : row.offsetHeight;
+  while ((row = patients.shift())) {
+    const h = row.offsetHeight;
 
     if (!page || page.offsetHeight + h > 700) {
-      if (page && row === rest) {
-        while (page.offsetHeight + 30 < 700) {
-          newTableBody.appendChild(emptyRow.cloneNode(true));
-        }
-      }
-
       newTable = table.cloneNode(true);
       newTableBody = newTable.children[2];
       newTableBody.innerHTML = "";
-
-      if (row === rest) {
-        newTable.children[1].innerHTML = "";
-      }
 
       page = document.createElement("div");
       page.appendChild(header.cloneNode(true));
@@ -60,34 +37,11 @@ export default function(win) {
       root.appendChild(page);
     }
 
-    if (row === rest) {
-      // 特殊情况（两行）
-      newTableBody.appendChild(tbody.removeChild(row.shift()));
-      newTableBody.appendChild(tbody.removeChild(row.shift()));
-      rest = null;
-
-      const shouldFillRows = newTableBody.children.length > 2; // 大于2行
-      //   && newTableBody.children[0].children.length > 1 // 第0行不是空行
-
-      // 移除占位用的空行
-      if (newTableBody.children[0].className === "empty-row") {
-        newTableBody.removeChild(newTableBody.children[0]);
-      }
-
-      if (shouldFillRows) {
-        const target = newTableBody.children[newTableBody.children.length - 2];
-
-        while (page.offsetHeight + 30 < 700) {
-          newTableBody.insertBefore(emptyRow.cloneNode(true), target);
-        }
-      }
-    } else {
-      // 病人信息
-      try {
-        tbody.removeChild(row);
-      } catch (error) {}
-      newTableBody.appendChild(row);
-    }
+    // 病人信息
+    try {
+      tbody.removeChild(row);
+    } catch (error) {}
+    newTableBody.appendChild(row);
   }
 
   root.removeChild(root.children[0]);
