1d85030f5cdf37113934f5ee05729eda5f3ae47a
wumin <18196569163@163.com>
2020-08-28 10:09:24 +0800

    陵城上下角标修改

--- src/Page/sheet-page/components/modal/special-modal2.vue
+++ src/Page/sheet-page/components/modal/special-modal2.vue
@@ -636,7 +636,7 @@ export default {
           text += allDoc[i];
         } else {
           if (GetLength(text) > okLength) {
-            text = text.replace(/ /g, "&nbsp;&nbsp;");
+            text = text.replace(/ /g, "&nbsp;");
             result.push(text);
             isSpecialLabel = false;
             text = allDoc[i];
@@ -651,7 +651,7 @@ export default {
         }
       }
       if (text) {
-        result.push(text.replace(/ /g, "&nbsp;&nbsp;"));
+        result.push(text.replace(/ /g, "&nbsp;"));
       }
       if (result.length == 0) {
         result.push("");
