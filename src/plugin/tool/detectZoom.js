3c23141bbda2678d0476055bf851b6e8a31bcf8d
liangjinmei <liangjinmei@cr-health.com>
2020-11-05 15:53:56 +0800

    缩放提示（对苹果电脑做处理）

--- src/plugin/tool/detectZoom.js
+++ src/plugin/tool/detectZoom.js
@@ -1,13 +1,14 @@
 // 获取屏幕缩放比 100 为正常
-export default function detectZoom (){ 
+export default function detectZoom (){
   var ratio = 0,
     screen = window.screen,
-    ua = navigator.userAgent.toLowerCase();
- 
+    ua = navigator.userAgent.toLowerCase(),
+    isMac = /macintosh|mac os x/i.test(navigator.userAgent);;
+
    if (window.devicePixelRatio !== undefined) {
-      ratio = window.devicePixelRatio;
+      ratio = isMac ? window.devicePixelRatio / 2 : window.devicePixelRatio;
   }
-  else if (~ua.indexOf('msie')) {  
+  else if (~ua.indexOf('msie')) {
     if (screen.deviceXDPI && screen.logicalXDPI) {
       ratio = screen.deviceXDPI / screen.logicalXDPI;
     }
@@ -15,10 +16,10 @@ export default function detectZoom (){
   else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
     ratio = window.outerWidth / window.innerWidth;
   }
-   
-   if (ratio){
+
+  if (ratio){
     ratio = Math.round(ratio * 100);
   }
-   
+
    return ratio;
 };
