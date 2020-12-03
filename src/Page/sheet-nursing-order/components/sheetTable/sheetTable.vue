7343e455d8eec248b845f941518b73aedf6d65d6
Dingsd <1162620357@qq.com>
2020-12-03 15:52:22 +0800

    feat： 花都 执行单 显示患者信息调整

--- src/Page/sheet-nursing-order/components/sheetTable/sheetTable.vue
+++ src/Page/sheet-nursing-order/components/sheetTable/sheetTable.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div
-      class="contant"
-      v-if="showSheetPage"
-      @click="upSheetPageIndex(index)"
-      :class="{active: sheetPageIndex == index}"
+        class="contant"
+        v-if="showSheetPage"
+        @click="upSheetPageIndex(index)"
+        :class="{active: sheetPageIndex == index}"
     >
       <!-- <img class="his-logo"
       src="../../../../common/images/his-logo/南医三院骨研院骨科医院logo.png" />-->
@@ -13,23 +13,28 @@
       <!-- <img class="his-logo"
       src="../../../../common/images/his-logo/山西孝义logo.png" />-->
       <div
-        style="
+          style="
             text-align: center;
             font-size: 18px;
             font-weight: bold;
         "
-      >{{ HOSPITAL_NAME_SPACE }}</div>
+      >{{ HOSPITAL_NAME_SPACE }}
+      </div>
       <div class="header-con">
-        <h1>{{sheetInfo.formTitle||"护嘱单"}}</h1>
+        {{ patientInfo }}
+        <h1>{{ sheetInfo.formTitle || "护嘱单" }}</h1>
         <p flex="main:justify">
-          <span>姓名：{{patientInfo.name}}</span>
-          <span>性别：{{patientInfo.sex}}</span>
-          <span>年龄：{{patientInfo.age}}</span>
-          <span>科室：{{patientInfo.deptName}}</span>
-          <span>入院日期：{{patientInfo.admissionDate | toymd}}</span>
-          <span>床号：{{patientInfo.bedLabel}}</span>
+          <span>姓名：{{ patientInfo.name }}</span>
+          <span>性别：{{ patientInfo.sex }}</span>
+          <span>年龄：{{ patientInfo.age }}</span>
+          <span>科室：{{ patientInfo.deptName }}</span>
+          <template v-show="!(HOSPITAL_NAME === '广州市花都区人民医院')">
+            <span>入院日期：{{ patientInfo.admissionDate | toymd }}</span>
+          </template>
+          <span>床号：{{ patientInfo.bedLabel }}</span>
           <!-- <span class="diagnosis-con" :title="patientInfo.diagnosis">诊断：{{patientInfo.diagnosis}}</span> -->
-          <span>住院号：{{patientInfo.inpNo}}</span>
+          <span v-if="HOSPITAL_NAME === '广州市花都区人民医院'">住院号：{{ patientInfo.patientId }}</span>
+          <span v-else>住院号：{{ patientInfo.inpNo }}</span>
           <!-- <span>入院日期：{{$route.query.admissionDate}}</span> -->
         </p>
       </div>
@@ -55,7 +60,8 @@
 
 .header-con {
   text-align: center;
-  width:100%
+  width: 100%
+
   h1 {
     font-size: 22px;
     color: #000;
@@ -87,6 +93,7 @@
   top: 21px;
   height: 44px;
 }
+
 .diagnosis-con
   max-width 340px
   overflow hidden
@@ -95,9 +102,10 @@
 </style>
 <script>
 import excel from "./components/excel/excel.vue";
-import { mapState, mapMutations } from "vuex";
+import {mapState, mapMutations} from "vuex";
 import bus from "vue-happy-bus";
 import sheetInfo from "../config/sheetInfo/index.js";
+
 export default {
   props: {
     data: Object,
@@ -120,25 +128,21 @@ export default {
     },
     title() {
       switch (sheetInfo.sheetType) {
-        case "jointnew":
-          {
-            return "护嘱单（关节外科）";
-          }
+        case "jointnew": {
+          return "护嘱单（关节外科）";
+        }
           break;
-        case "gynecologynew":
-          {
-            return "护嘱单（妇科）";
-          }
+        case "gynecologynew": {
+          return "护嘱单（妇科）";
+        }
           break;
-        case "endocrinologynew":
-          {
-            return "护嘱单（内分泌科）";
-          }
+        case "endocrinologynew": {
+          return "护嘱单（内分泌科）";
+        }
           break;
-        case "neurology":
-          {
-            return "护嘱单（神经内科）";
-          }
+        case "neurology": {
+          return "护嘱单（神经内科）";
+        }
           break;
         default:
           return "护嘱单-";
@@ -169,8 +173,10 @@ export default {
       return new Date(val).Format("yyyy-MM-dd");
     }
   },
-  created() {},
-  destroyed() {} /* fix vue-happy-bus bug */,
+  created() {
+  },
+  destroyed() {
+  } /* fix vue-happy-bus bug */,
   components: {
     excel
   }
