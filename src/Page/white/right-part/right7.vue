b70f018ffb25b1667d64a2f128319ea08e41643c
liangjinmei <liangjinmei@cr-health.com>
2021-04-21 18:31:59 +0800

    ✨feat: 聊城二院白板新增安全指标功能

---
+++ src/Page/white/right-part/right7.vue
@@ -0,0 +1,141 @@
+<template>
+  <div style="padding-bottom: 20px;">
+    <boxBase title="安全日志指标" :icon="require('../images/留言板.png')">
+      <div class="body-con"
+           v-loading="pageLoading"
+           slot="body-con">
+           <div class="group" style="padding-bottom: 20px;">
+             <label>项目</label>
+             <label>安全天数</label>
+           </div>
+           <div class="group" v-for="item in list" :key="item.prop">
+             <label>{{item.name}}</label>
+             <div class="right">
+                <div class="day">
+                  {{listData[item.prop] || 0}}
+                </div>
+                <div class="date" v-if="item.prop != 'total'">
+                  <el-date-picker
+                    v-model="allDate[item.prop]"
+                    type="date"
+                    placeholder="选择日期" @change="changeDate($event,item.prop)">
+                  </el-date-picker>
+                </div>
+             </div>
+           </div>
+      </div>
+    </boxBase>
+  </div>
+</template>
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+.body-con
+  margin 12px 10px 12px 20px
+  min-height 220px
+  .group
+    display flex
+    justify-content space-between
+    align-items center
+    padding-bottom 5px
+    .right
+      position relative
+      .el-input
+        width 85px
+      >>>.el-input__inner
+        height 30px
+        line-height 30px
+        border: none;
+      .day
+        width 50px
+        height 30px
+        font-size 16px
+        text-align center
+        color #4bb08d
+        line-height 30px
+        font-weight bold
+        padding-right 35px
+      .date
+        position absolute
+        top 0
+        right 0
+        >>>.el-input__inner
+          opacity 0
+
+</style>
+<script>
+import boxBase from '../base/box-base.vue'
+import slider from './slider/slider.vue'
+import { getWhiteLogIndicators,saveWhiteLogIndicators } from '../api'
+import common from '@/common/mixin/common.mixin.js'
+import bus from 'vue-happy-bus'
+export default {
+  mixins: [common],
+  data() {
+    return {
+      bus: bus(this),
+      pageLoading: false,
+      list: [
+        {
+          name: '院内压力性损伤',
+          prop: 'ylxss'
+        },
+        {
+          name: '跌倒坠床',
+          prop: 'ddzc'
+        },
+        {
+          name: '管理滑脱',
+          prop: 'glht'
+        },
+        {
+          name: '安全用药',
+          prop: 'aqyy'
+        },
+        {
+          name: '总天数',
+          prop: 'total'
+        },
+      ],
+      listData: {},
+      allDate: {}
+    }
+  },
+  created() {
+    this.bus.$on('indexGetAllData', this.getData)
+  },
+  methods: {
+    // 查询安全指标
+    getData() {
+      this.pageLoading = true
+      getWhiteLogIndicators(this.deptCode).then(res => {
+        this.listData = res.data.data || {};
+        this.pageLoading = false
+      })
+    },
+    // 新增和修改安全指标
+    // {
+    // 	"deptCode":"",
+    // 	"ylxssDate":"院内压力性损伤日期",
+    // 	"ddzcDate":"跌倒坠床日期",
+    // 	"glhtDate":"管理滑脱日期",
+    // 	"aqyyDate":"安全用药日期"
+    // }
+    changeDate(val,prop){
+      let data = {
+        deptCode: this.deptCode,
+        ylxssDate: this.listData.ylxssDate,
+        ddzcDate: this.listData.ddzcDate,
+        glhtDate: this.listData.glhtDate,
+        aqyyDate: this.listData.aqyyDate
+      }
+      data[`${prop}Date`] = val;
+      saveWhiteLogIndicators(data).then(res=>{
+        this.getData();
+      })
+    }
+
+  },
+  components: {
+    boxBase, slider
+  }
+}
+</script>
