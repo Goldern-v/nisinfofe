92f2be58ea946b7bd2aac74d723015e0c27e6331
小高 <8555921+gaohaixiong@user.noreply.gitee.com>
2021-08-16 10:23:52 +0800

    修改曲周问题

--- src/Page/sheet-page/components/config/common_hl/th.js
+++ src/Page/sheet-page/components/config/common_hl/th.js
@@ -6,316 +6,316 @@ export default {
   deptType: '',
   th: {
     top: [{
-        name: '日期',
-        style: {
-          minWidth: '36px',
-          maxWidth: '36px',
-        },
-        colspan: '1',
-        rowspan: '3'
+      name: '日期',
+      style: {
+        minWidth: '36px',
+        maxWidth: '36px',
       },
-      {
-        name: '时间',
-        style: {
-          minWidth: '36px',
-          maxWidth: '36px',
-        },
-        colspan: '1',
-        rowspan: '3'
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: '时间',
+      style: {
+        minWidth: '36px',
+        maxWidth: '36px',
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         体 <br/> 温 <br/>℃
         `,
-        colspan: '1',
-        rowspan: '3',
-        style: {
-          whiteSpace: 'nowrap',
-          minWidth: '34px',
-        }
-      },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3',
+      style: {
+        whiteSpace: 'nowrap',
+        minWidth: '34px',
+      }
+    },
+    {
+      name: `
         脉 <br/> 搏 <br/> 次/分
         `,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        style: {
-          whiteSpace: 'nowrap',
-          minWidth: '38px',
-        }
-      },
-      {
-        name: `
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      style: {
+        whiteSpace: 'nowrap',
+        minWidth: '38px',
+      }
+    },
+    {
+      name: `
         呼<br/>吸<br/>次/分
         `,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        style: {
-          whiteSpace: 'nowrap',
-          minWidth: '38px',
-        }
-      },
-      {
-        name: `
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      style: {
+        whiteSpace: 'nowrap',
+        minWidth: '38px',
+      }
+    },
+    {
+      name: `
         血<br/>压<br/>mmHg
         `,
-        style: {
-          minWidth: '52px',
-          maxWidth: '52px',
-          whiteSpace: 'nowrap'
-        },
-        colspan: '1',
-        rowspan: '3'
+      style: {
+        minWidth: '52px',
+        maxWidth: '52px',
+        whiteSpace: 'nowrap'
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         血氧<br/>饱和<br/>度<br/>(%)
         `,
-        style: {
-          minWidth: '38px',
-          maxWidth: '38px',
-          whiteSpace: 'nowrap'
-        },
-        colspan: '1',
-        rowspan: '3'
+      style: {
+        minWidth: '38px',
+        maxWidth: '38px',
+        whiteSpace: 'nowrap'
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         神<br/>志
         `,
-        style: {
-          minWidth: '50px',
-          maxWidth: '50px'
-        },
-        colspan: '1',
-        rowspan: '3'
+      style: {
+        minWidth: '50px',
+        maxWidth: '50px'
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         瞳孔
         `,
-        style: {},
-        colspan: '4',
-        rowspan: '1'
-      },
-      {
-        name: `
+      style: {},
+      colspan: '4',
+      rowspan: '1'
+    },
+    {
+      name: `
         入量
         `,
-        colspan: '2',
-        rowspan: '1',
-        canSet: false,
-      },
-      {
-        name: `
+      colspan: '2',
+      rowspan: '1',
+      canSet: false,
+    },
+    {
+      name: `
         出量
         `,
-        colspan: '2',
-        rowspan: '1',
-        canSet: false,
-      },
-      {
-        name: `标题1`,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        canSet: true,
-        key: 'fieldOne',
-        style: {
-          minWidth: '37px',
-          maxWidth: '37px',
-        }
-      },
-      {
-        name: `标题2`,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        canSet: true,
-        key: 'fieldTwo',
-        style: {
-          minWidth: '37px',
-          maxWidth: '37px',
-        }
-      },
-      {
-        name: `标题3`,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        canSet: true,
-        key: 'fieldThree',
-        style: {
-          minWidth: '37px',
-          maxWidth: '37px',
-        }
-      },
-      {
-        name: `标题4`,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        canSet: true,
-        key: 'fieldFour',
-        style: {
-          minWidth: '37px',
-          maxWidth: '37px',
-        }
-      },
-      {
-        name: `标题5`,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        canSet: true,
-        key: 'fieldFive',
-        style: {
-          minWidth: '37px',
-          maxWidth: '37px',
-        }
-      },
-      {
-        name: `标题6`,
-        style: {},
-        colspan: '1',
-        rowspan: '3',
-        canSet: true,
-        key: 'fieldSix',
-        style: {
-          minWidth: '37px',
-          maxWidth: '37px',
-        }
-      },
-      {
-        name: `
+      colspan: '2',
+      rowspan: '1',
+      canSet: false,
+    },
+    {
+      name: `标题1`,
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      canSet: true,
+      key: 'fieldOne',
+      style: {
+        minWidth: '37px',
+        maxWidth: '37px',
+      }
+    },
+    {
+      name: `标题2`,
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      canSet: true,
+      key: 'fieldTwo',
+      style: {
+        minWidth: '37px',
+        maxWidth: '37px',
+      }
+    },
+    {
+      name: `标题3`,
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      canSet: true,
+      key: 'fieldThree',
+      style: {
+        minWidth: '37px',
+        maxWidth: '37px',
+      }
+    },
+    {
+      name: `标题4`,
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      canSet: true,
+      key: 'fieldFour',
+      style: {
+        minWidth: '37px',
+        maxWidth: '37px',
+      }
+    },
+    {
+      name: `标题5`,
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      canSet: true,
+      key: 'fieldFive',
+      style: {
+        minWidth: '37px',
+        maxWidth: '37px',
+      }
+    },
+    {
+      name: `标题6`,
+      style: {},
+      colspan: '1',
+      rowspan: '3',
+      canSet: true,
+      key: 'fieldSix',
+      style: {
+        minWidth: '37px',
+        maxWidth: '37px',
+      }
+    },
+    {
+      name: `
         特殊情况记录
         `,
-        style: {
-          minWidth: "260px",
-          maxWidth: "260px"
-        },
-        colspan: '1',
-        rowspan: '3'
+      style: {
+        minWidth: "260px",
+        maxWidth: "260px"
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '3'
+    },
+    {
+      name: `
         签名
         `,
-        style: {},
-        colspan: '1',
-        rowspan: '3'
-      },
+      style: {},
+      colspan: '1',
+      rowspan: '3'
+    },
     ],
     mid: [{
-        name: `
+      name: `
         大小
         `,
-        style: {
-          whiteSpace: 'nowrap'
-        },
-        colspan: '2',
-        rowspan: '1'
+      style: {
+        whiteSpace: 'nowrap'
       },
-      {
-        name: `
+      colspan: '2',
+      rowspan: '1'
+    },
+    {
+      name: `
         反射
         `,
-        colspan: '2',
-        rowspan: '1'
-      },
-      {
-        name: `
+      colspan: '2',
+      rowspan: '1'
+    },
+    {
+      name: `
         内容
         `,
-        style: {
-          minWidth: '52px',
-          maxWidth: '52px'
-        },
-        colspan: '1',
-        rowspan: '2'
+      style: {
+        minWidth: '52px',
+        maxWidth: '52px'
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `
         量<br/>(ml)
         `,
-        colspan: '1',
-        rowspan: '2',
-        style: {
-          minWidth: '28px',
-          maxWidth: '28px',
-        },
+      colspan: '1',
+      rowspan: '2',
+      style: {
+        minWidth: '38px',
+        maxWidth: '38px',
       },
-      {
-        name: `
+    },
+    {
+      name: `
         内容
         `,
-        style: {
-          minWidth: '52px',
-          maxWidth: '52px'
-        },
-        colspan: '1',
-        rowspan: '2'
+      style: {
+        minWidth: '52px',
+        maxWidth: '52px'
       },
-      {
-        name: `
+      colspan: '1',
+      rowspan: '2'
+    },
+    {
+      name: `
         量<br/>(ml)
         `,
-        colspan: '1',
-        rowspan: '2',
-        style: {
-          minWidth: '28px',
-          maxWidth: '28px',
-          whiteSpace: 'nowrap'
-        },
+      colspan: '1',
+      rowspan: '2',
+      style: {
+        minWidth: '38px',
+        maxWidth: '38px',
+        whiteSpace: 'nowrap'
       },
+    },
     ],
     bottom: [{
-        name: `
+      name: `
         左<br/>(mm)
         `,
-        colspan: '1',
-        rowspan: '1',
-        style: {
-          minWidth: '25px',
-          whiteSpace: 'nowrap'
-        },
+      colspan: '1',
+      rowspan: '1',
+      style: {
+        minWidth: '25px',
+        whiteSpace: 'nowrap'
       },
-      {
-        name: `
+    },
+    {
+      name: `
         右<br/>(mm)
         `,
-        colspan: '1',
-        rowspan: '1',
-        style: {
-          minWidth: '25px',
-          whiteSpace: 'nowrap'
-        },
+      colspan: '1',
+      rowspan: '1',
+      style: {
+        minWidth: '25px',
+        whiteSpace: 'nowrap'
       },
-      {
-        name: `
+    },
+    {
+      name: `
         左
         `,
-        colspan: '1',
-        rowspan: '1',
-        style: {
-          minWidth: '25px',
-          whiteSpace: 'nowrap'
-        },
+      colspan: '1',
+      rowspan: '1',
+      style: {
+        minWidth: '25px',
+        whiteSpace: 'nowrap'
       },
-      {
-        name: `
+    },
+    {
+      name: `
         右
         `,
-        colspan: '1',
-        rowspan: '1',
-        style: {
-          minWidth: '25px',
-          whiteSpace: 'nowrap'
-        },
+      colspan: '1',
+      rowspan: '1',
+      style: {
+        minWidth: '25px',
+        whiteSpace: 'nowrap'
       },
+    },
     ]
   }
 }
