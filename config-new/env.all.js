0dd29fda94a3564659a6d6b77275d4c23096be43
陶星宇 <2609813922@qq.com>
2021-09-09 09:28:39 +0800

    南方中西医

--- config-new/env.all.js
+++ config-new/env.all.js
@@ -101,8 +101,9 @@ module.exports = {
   },
   "nanfangzhongxiyi": {
     HOSPITAL_ID: '"nanfangzhongxiyi"',
-    HOSPITAL_NAME: '"南方中西医"',
-    HOSPITAL_NAME_SPACE: '"南 方 中 西 医"'
+    HOSPITAL_NAME: '"南方医科大学中西医结合医院"',
+    HOSPITAL_NAME_SPACE: '"南 方 医 科 大 学 中 西 医 结 合 医 院"',
+    title: '"南方医科大学中西医结合医院智慧护理信息系统"'
   },
   "yangchunzhongyi": {
     HOSPITAL_ID: '"yangchunzhongyi"',
