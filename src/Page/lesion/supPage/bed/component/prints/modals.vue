d53635e27f442cce0fea6ec754453a4aa5d7e472
玛卡巴卡 <1499487569@qq.com>
2021-09-09 08:30:58 +0800

    【花都】腕带批量打印过敏信息字段调整

--- src/Page/lesion/supPage/bed/component/prints/modals.vue
+++ src/Page/lesion/supPage/bed/component/prints/modals.vue
@@ -337,10 +337,10 @@
             <div class="allergy">
               <p class="gm">
                 过敏信息：
-                <span v-if="allergy1">{{ allergy1 }};</span>
-                <span v-if="drugGms">{{ drugGms }};</span>
-                <span v-if="allergy2">{{ allergy2 }}</span>
-                <span v-if="!(allergy1||drugGms||allergy2)">无</span>
+                <span v-if="item.allergy1">{{ item.allergy1 }};</span>
+                <span v-if="item.drugGms">{{ item.drugGms }};</span>
+                <span v-if="item.allergy2">{{ item.allergy2 }}</span>
+                <span v-if="!(item.allergy1||item.drugGms||item.allergy2)">无</span>
               </p>
             </div>
             <!-- <svg id="barcode"></svg> -->
