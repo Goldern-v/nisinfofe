d2a05b00020edee661713f0bb9584e20e807d14d
guimin_peng <guimin_peng@kdyl.com>
2021-09-29 17:58:57 +0800

    fix: 横沥-床位一览卡-妇产科-床位显示调整（姓名带B的不统计）

--- src/Page/lesion/supPage/bed/component/search-con/search-con.vue
+++ src/Page/lesion/supPage/bed/component/search-con/search-con.vue
@@ -315,10 +315,22 @@ export default {
     },
     // 全部床位
     allBed() {
-      return this.bedList;
+      if (this.HOSPITAL_ID === 'hengli' && this.$store.state.lesion.deptCode === '103') {
+        return this.bedList.filter((item) => item.name.indexOf("B") === -1);
+      } else {
+        return this.bedList;
+      }
     },
     nullBed() {
       return this.bedList.filter((item) => !item.patientId);
+    },
+    inBedLength() {
+      if (this.HOSPITAL_ID === 'hengli' && this.$store.state.lesion.deptCode === '103') {
+        console.log(this.bedList, 88)
+        return this.bedList.filter((item) => item.patientId && item.name.indexOf("B") === -1);
+      } else {
+        return this.bedList.filter((item) => item.patientId);
+      }
     },
     inBed() {
       return this.bedList.filter((item) => item.patientId);
@@ -460,7 +472,7 @@ export default {
         },
         {
           name: "在床",
-          num: this.inBed.length,
+          num: this.inBedLength.length,
           type: "bed",
         },
         //  {
@@ -605,6 +617,7 @@ export default {
         this.levelColor = levelColor;
         patients(this.deptCode).then((res) => {
           this.bedList = res.data.data.map((item) => {
+            console.log(item.name, 978)
             item.nursingClassColor = (
               levelColor.find((o) => o.code == item.nursingClass) || {}
             ).name;
@@ -694,7 +707,8 @@ export default {
         case "":
         case "全部床位":
           {
-            this.$parent.bedList = this.allBed;
+            // this.$parent.bedList = this.allBed;
+            this.$parent.bedList = this.bedList
           }
           break;
         case "空床":
