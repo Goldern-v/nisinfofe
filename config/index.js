e186fe50b2bb1137fcfe03fa12dba17b613ba3bd
小高 <8555921+gaohaixiong@user.noreply.gitee.com>
2021-10-09 18:06:36 +0800

    修改妇幼正式服地址

--- config/index.js
+++ config/index.js
@@ -71,19 +71,14 @@ const currentTargetUrl = (() => {
     /** 聊城 */
     case "liaocheng":
       // return "http://120.224.211.7:9094" //聊城正式
-<<<<<<< HEAD
-      // return "https://info.cr-health.com:20209" // 公司-聊城测试-外网
-      return "http://192.168.1.54:9871" //公司-聊城测试-内网
-=======
       return "https://info.cr-health.com:20209" // 公司-聊城测试-外网
       // return "http://192.168.1.54:9871" //公司-聊城测试-内网
->>>>>>> 0504dc7e45d1c2340b30309a98cde308acfeec26
 
     /** 江门妇幼 */
     case "fuyou":
-      return "http://218.14.180.38:9094" // 江门妇幼正式
+      // return "http://218.14.180.38:9094" // 江门妇幼正式
       // return "https://info.cr-health.com:20211" // 公司-江门妇幼测试-外网
-      // return "http://192.168.1.54:9872" // 公司-江门妇幼测试-内网
+      return "http://192.168.1.54:9872" // 公司-江门妇幼测试-内网
 
     /** 中山七 */
     case "zhongshanqi":
