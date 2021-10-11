c7ebb6f232c35c881f183db70cc021cdfd24b1d8
guimin_peng <guimin_peng@kdyl.com>
2021-10-11 15:13:36 +0800

    fix: 北海-记录单问题

--- config-new/prod.env.js
+++ config-new/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  HOSPITAL_ID: '"hengli"',
-  HOSPITAL_NAME: '"东莞市横沥医院"',
-  HOSPITAL_NAME_SPACE: '"东 莞 市 横 沥 医 院"'
+  HOSPITAL_ID: '"fuyou"',
+  HOSPITAL_NAME: '"江门市妇幼保健院"',
+  HOSPITAL_NAME_SPACE: '"江 门 市 妇 幼 保 健 院"'
 };
