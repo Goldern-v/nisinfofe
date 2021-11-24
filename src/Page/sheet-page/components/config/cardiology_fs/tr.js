4f9962a350f288ccc8a8bb04d36071e8470350eb
TXY <2609813922@qq.com>
2021-11-24 15:14:01 +0800

    护记多选自定义符号分隔

--- src/Page/sheet-page/components/config/cardiology_fs/tr.js
+++ src/Page/sheet-page/components/config/cardiology_fs/tr.js
@@ -1,6 +1,21 @@
-/*
+/** 
 佛山市一 - 心内科通用护理记录单
 */
+
+/**
+ * @description: 开发注意事项:
+    1、textarea: { width:45 }, change: (e, td) => limitChange(e, td, 6) 的意思是宽度设置45，字数限制6个。
+    2、autoComplete: { data: [] } 为下拉框内容，根据需要添加。
+    3、splice: '' 为下拉框多选功能，搭配autoComplete使用, (''内容可自定义,splice: '/'为用“/”分隔)。
+    4、name、next 为双击弹窗的内容，name为标题，next是单位。
+    5、自定义项目，在th里要写 canSet: true , key: '' , key值为th对应tr的key值。
+    6、{ key: "sign", value: "" } { hidden: false, key: 'signerNo', value: ''} 单签
+        { key: "sign2", value: "" } { hidden: false, key: 'signerNo2', value: ''} 双签
+        { key: "audit", value: "" } { hidden: false, key: 'auditorNo', value: ''} 审核签名
+        当开启多个签名功能时，记得将hidden变为false，如果是true, 就会被隐藏,根据需要使用。
+    7、多个签名时还需要在sheet-print-page.vue和excel.vue里配置打印样式。
+*/
+
 import {
   multiDictInfo
 } from "../../../api/index";
@@ -16,7 +31,7 @@ import {
 } from "../keyEvent/date";
 
 export default [
-  { key: "recordDate", value: "", hidden:true},
+  { key: "recordDate", value: "", hidden:true },
   { key: "recordMonth", event: event_date, click: click_date, value: ''},
   { key: "recordHour", event: event_time, value: ''},
   { key: 'temperature', event: keyf1,value: '',name: '体温',next: '℃',textarea:{ width:35},change: (e, td) => limitChange(e, td, 4)},
