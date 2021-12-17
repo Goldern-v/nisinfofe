aee984639b185874eb238b46ed080c81790dc15c
hsenap <1244696102@qq.com>
2021-12-17 17:33:09 +0800

    [武汉肺科]部署

--- config/env.all.js
+++ config/env.all.js
@@ -162,5 +162,13 @@ module.exports = {
     COMPANY_NAME: '"宸瑞普惠(广州)科技有限公司"',
     ABOUT_INFO: '"关于宸瑞普惠"',
     title: '"智慧护理信息系统"'
+  },
+  whfk: {
+    HOSPITAL_ID: '"whfk"',
+    HOSPITAL_NAME: '"武汉市肺科医院"',
+    HOSPITAL_NAME_SPACE: '"武 汉 市 肺 科 医 院（武汉市结核病防治所）"',
+    COMPANY_NAME: '"宸瑞普惠(广州)科技有限公司"',
+    ABOUT_INFO: '"关于宸瑞普惠"',
+    title: '"智慧护理信息系统"'
   }
 };
