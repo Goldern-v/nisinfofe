08eaa9a28eb963fb9c0120b1b5fb4fd05c8fb8d9
玛卡巴卡 <1499487569@qq.com>
2021-12-22 09:30:55 +0800

    【聊城】护理巡视模块开放

--- src/components/HLheader/LiaoChengHeader.vue
+++ src/components/HLheader/LiaoChengHeader.vue
@@ -189,6 +189,11 @@
                 <i class="iconfont icon-jiaobanzhi"></i> 交班报告
               </el-row>
             </router-link>
+            <router-link to="/nursingRounds" tag="span">
+              <el-row class="nav-item" type="flex" align="middle"
+                >护理巡视
+              </el-row>
+            </router-link>
             <router-link to="/board" tag="span">
               <el-row class="nav-item" type="flex" align="middle">
                 <i class="iconfont icon-baiban"></i> 白板
