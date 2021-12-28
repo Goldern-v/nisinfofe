042e2f2eedeef045ee27e2f35e126cf7d45942c1
hsenap <1244696102@qq.com>
2021-12-28 15:36:49 +0800

    [佛山市一]pad扫码调整

--- src/components/HLheader/modal/printQrCode.vue
+++ src/components/HLheader/modal/printQrCode.vue
@@ -133,7 +133,7 @@ export default {
     //二维码
     qrcode() {
       let titleObject = this.userName + " " + this.passWord;
-      ['foshanrenyi'].includes(this.HOSPITAL_ID ) && (titleObject=JSON.stringify({ user: this.userName,password: this.passWord}));  
+      ['foshanrenyi'].includes(this.HOSPITAL_ID ) && (titleObject=this.userName + " " + md5(this.passWord));  
       let qrcode = new QRCode(this.$refs.qrcodeContainer, {
         // width: this.cmpx,// 二维码的宽
         // height: this.cmpx,// 二维码的高
