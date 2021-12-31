3ef2b9ce165894d31809b9b6aa46958ed6748a09
chentingqi <1051570119@qq.com>
2021-12-31 15:51:50 +0800

    [武警]:更换体温单记录接口

--- src/Page/temperature-chart/new-singleTemperature-chart-wujing/components/tab-con.vue
+++ src/Page/temperature-chart/new-singleTemperature-chart-wujing/components/tab-con.vue
@@ -365,6 +365,7 @@ import moment from "moment";
 import nullBg from "../../../../components/null/null-bg";
 import {
   getVitalSignListBy10,
+  getVitalSignListByDate,
   getmultiDict,
   getfieldList,
   savefieldTitle,
@@ -522,9 +523,11 @@ export default {
       };
       await this.getVitalList();
       /* 获取患者某个时间点的体征信息 */
-      await getVitalSignListBy10({
+      await getVitalSignListByDate({
         visitId: data.visitId,
         patientId: data.patientId,
+         wardCode: this.patientInfo.wardCode,
+        recordDate: moment(new Date(this.query.entryDate)).format("YYYY-MM-DD"),
       }).then((res) => {
         res.data.data.map((item, index) => {
           /* 如果该患者没有体温单记录则返回 */
