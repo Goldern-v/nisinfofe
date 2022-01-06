c4392928470b17566caf6a2840c9035e16868c69
TXY <2609813922@qq.com>
2022-01-06 11:23:41 +0800

    护理记录单patientInfo从主表信息中获取

--- src/Page/sheet-page/components/sheetTable-oxytocin/sheetTable.vue
+++ src/Page/sheet-page/components/sheetTable-oxytocin/sheetTable.vue
@@ -402,7 +402,7 @@ export default {
   computed: {
     patientInfo() {
       // return this.sheet.patientInfo
-      return this.sheetInfo.selectBlock || {};
+      return this.sheetInfo.masterInfo || {};
     },
     /** 只读模式 */
     readOnly() {
