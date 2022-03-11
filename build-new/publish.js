b2b20117290b440e6af0e0632175a735a919f86d
jack <wen_yongjie@126.com>
2022-03-11 15:33:54 +0800

    fix：添加福清测试服

--- build-new/publish.js
+++ build-new/publish.js
@@ -35,7 +35,8 @@ let sshUpload = (
     武汉亚心医院:"wuhanyaxin",
     武汉市肺科医院:"wuhanfeike",
     佛山市顺德区龙江医院:"shundelongjiang",
-    南方医科大学顺德医院附属杏坛医院: "fsxt"
+    南方医科大学顺德医院附属杏坛医院: "fsxt",
+    福清妇幼保健院:"fuqingfuyou"
   };
   let hospitalEN = "dongguanhoujie";
   if (hospitalList[hospitalName]) {
