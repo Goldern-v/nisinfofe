ee7cacbbb506897dde4ccdadaed9f49b18f88268
lyplyplyp <1229515909@qq.com>
2022-03-23 18:07:15 +0800

    顺德龙江护理记录单调整

--- src/Page/sheet-page/components/utils/sheetRow.js
+++ src/Page/sheet-page/components/utils/sheetRow.js
@@ -65,7 +65,7 @@ export function getRowNum(index) {
     }
   } else if (sheetInfo.sheetType === 'blood_circulation_hl') {
     return 35
-  } else if (sheetInfo.sheetType === 'criticaldisease_xt' || sheetInfo.sheetType === 'cardiovascular_xt') {
+  } else if (sheetInfo.sheetType === 'criticaldisease_xt' || sheetInfo.sheetType === 'cardiovascular_xt' || sheetInfo.sheetType === 'neonate_sdlj') {
     return 20
   } else if (sheetInfo.sheetType === 'access_fk'||sheetInfo.sheetType === 'cardiac_lcey') {
     return 28
