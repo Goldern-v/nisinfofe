fd5c205469986c549317b12c688f3edfa1cf457c
HeJinYin <328869170@qq.com>
2022-05-13 09:12:06 +0800

    【武汉肺科】 患者资料接口字段修改

--- src/Page/patientInfo/supPage/doctorEmr/components/tree/tree.vue
+++ src/Page/patientInfo/supPage/doctorEmr/components/tree/tree.vue
@@ -86,7 +86,7 @@ export default {
             let resDataArr = res[0].data.data;
             let keys = Object.keys(resDataArr);
             let regions = [];
-            if (this.HOSPITAL_ID == "lingcheng" || this.HOSPITAL_ID == "shannan" || this.HOSPITAL_ID == "quzhou"||this.HOSPITAL_ID == "whfk") {
+            if (this.HOSPITAL_ID == "lingcheng" || this.HOSPITAL_ID == "shannan" || this.HOSPITAL_ID == "quzhou") {
               for (let i = 0; i < keys.length; i++) {
                 regions.push({
                   label: keys[i],
