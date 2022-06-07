5e5e0a06f25eb8cdfcef5f95dd38e08e3e42f5ad
pgm <1796621138@qq.com>
2022-06-07 14:26:55 +0800

    fix: 护理记录单-谢岗和南方中西医修改权限校验问题

--- src/Page/sheet-page/sheet-page.vue
+++ src/Page/sheet-page/sheet-page.vue
@@ -298,6 +298,9 @@ import { decodeRelObj } from "./components/utils/relObj";
 import { sheetScrollBotton } from "./components/utils/scrollBottom";
 import { blockSave, getNurseExchageInfo } from "./api/index";
 import TableRadioVue from './components/sheetTable/components/table-components/TableRadio.vue';
+import { getRowNum } from "./components/utils/sheetRow"
+
+
 export default {
   mixins: [common],
   data() {
@@ -382,7 +385,7 @@ export default {
             if(index==0){
               nowX=  x
             }else{
-              nowX= 16 + 17*(index-1) + x+1
+              nowX= (getRowNum(index) - 1) + getRowNum(index)*(index-1) + x + 1
             }
             tr.isRead = this.isRead(tr,x,nowX);
             tr.map((td, y) => {
@@ -503,17 +506,22 @@ export default {
         return true
       }
       if (
-        (this.HOSPITAL_ID == "huadu" &&
+        this.HOSPITAL_ID == "huadu" &&
         sheetInfo.sheetType === "body_temperature_Hd" &&
         td &&
-        this.listData[x])
-        || 
-        (this.HOSPITAL_ID == "xiegang" && td && this.listData[x])
-        ||
-        (this.HOSPITAL_ID == "nanfangzhongxiyi" && td && this.listData[x])
+        this.listData[x]
       ) {
         return !this.listData[x].canModify;
       }
+
+      if (
+        (this.HOSPITAL_ID == "xiegang" && td && this.listData[nowX])
+        ||
+        (this.HOSPITAL_ID == "nanfangzhongxiyi" && td && this.listData[nowX])
+      ) {
+        return !this.listData[nowX].canModify;
+      }
+
       if (td && td.key == "recordYear") {
         if (!tr.find((item) => item.key == "recordMonth").value) {
           td.value = "";
