eb77d7c2285b3795d795519357c0398b5025ae1d
小高 <8555921+gaohaixiong@user.noreply.gitee.com>
2022-06-24 14:19:04 +0800

    体温单跨域更改

--- src/Page/temperature-chart/new-singleTemperature-chart-nanfangzhongxiyi/components/temperatureNew.vue
+++ src/Page/temperature-chart/new-singleTemperature-chart-nanfangzhongxiyi/components/temperatureNew.vue
@@ -96,7 +96,8 @@ export default {
       printAllPath: "",
       intranetUrl:
         // "http://localhost:8080/#/" /* 医院正式环境内网 导致跨域 */,
-        "http://192.168.1.78:8080/#/" /* 医院正式环境内网 导致跨域 */,
+        // "http://192.168.1.78:8080/#/" /* 医院正式环境内网 导致跨域 */,
+        "http://10.158.210.28:9093/temperature/#/" /* 医院正式环境内网 导致跨域 */,
       printAllUrl:
         "http://10.158.210.28:9093/temperature/#/printAll" /* 医院正式环境内网批量打印 */,
       outNetUrl:
