b7da98be43f11f8e510b030be595eb6dc2ee444c
jack <wen_yongjie@126.com>
2022-07-28 11:28:41 +0800

    fix:杏坛屏蔽不良事件，龙江修改名称、临邑修改床位一览卡

--- src/components/HLheader/FSXTHeader.vue
+++ src/components/HLheader/FSXTHeader.vue
@@ -465,13 +465,13 @@
                   </el-row>
                 </el-dropdown-item> -->
 
-                <el-dropdown-item :class="{active: $route.path == '/badEvent'}">
+                <!-- <el-dropdown-item :class="{active: $route.path == '/badEvent'}">
                   <router-link to="/badEvent" tag="span">
                     <el-row class="menu-item" type="flex" align="middle">
                       <i class="badEvent"></i>不良事件
                     </el-row>
                   </router-link>
-                </el-dropdown-item>
+                </el-dropdown-item> -->
                 <!-- <el-dropdown-item :class="{active: $route.path == '/nursingRules'}">
                   <router-link to="/nursingRules" tag="span">
                     <el-row class="menu-item" type="flex" align="middle">
