ee2acdaa08927eb80db070d0496683e093eeca6a
Mr.Zeng <10189773+zeng-seng@user.noreply.gitee.com>
2022-07-31 00:41:38 +0800

    【临邑】入院评估修改

--- config/index.js
+++ config/index.js
@@ -186,7 +186,8 @@ const currentTargetUrl = (() => {
     case 'lyxrm':
       // return "http://192.168.1.54:9890" // 内网测试
       // return "http://222.133.53.230:9093" // 临邑正式
-      return "http://222.133.53.230:9091" // 临邑正式
+      // return "http://222.133.53.230:9091" // 临邑正式
+      return "http://120.24.240.231:19097"  // 临邑正式(临邑本地)
     // 广东同江医院
     case 'gdtj':
       return 'http://192.168.1.54:9866'
