1dd480ba8973d54cbe5f8c5c162f7f6badee7a85
hgy <1309843131@qq.com>
2022-08-25 16:05:37 +0800

    fix: 临邑-护记增加同步医嘱;福清-不良事件查询修改

---
+++ src/Page/sheet-page/components/modal/advice-modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <sweet-modal ref="modalName" title="同步医嘱" class="modal-con">
+    <advice ref="advice" :isMulti="true" @handleCheckbox="handleCheckbox" />
+    <div slot="button">
+      <el-button class="modal-btn" @click="$refs.modalName.close()"
+        >取消</el-button
+      >
+      <el-button class="modal-btn" type="primary" @click="post">确认</el-button>
+    </div>
+  </sweet-modal>
+</template>
+<style lang="stylus" scoped>
+.modal-con
+  >>>.sweet-content
+    max-height 100vh
+  >>>.content
+    overflow hidden
+    margin: 0 0 10px
+  >>>.sweet-content
+    background #F0f0f0
+</style>
+<script>
+import advice from "@/Page/patientInfo/supPage/advice/advice";
+export default {
+  props: {},
+  data() {
+    return {
+      selected: []
+    };
+  },
+  methods: {
+    open() {
+      this.selected = []
+      this.$refs.modalName.open();
+      this.$refs.advice.getData()
+    },
+    post() {
+      if (this.selected.length == 0) return this.$message.warning('请选择一条数据')
+      this.$emit('handleOk',{ item: this.selected.map(v => ({val: [v.orderText, v.dosageUnits, v.administration].join('-')})), key: 'val' })
+      this.$refs.modalName.close()
+    },
+    handleCheckbox(e) {
+      console.log('test-e', e)
+      this.selected = e
+    }
+  },
+  components: {
+    advice
+  }
+};
+</script>
