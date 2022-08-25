97910ae46f7e2469a79d0a5b80b1d5f1781dab74
HeJinYin <328869170@qq.com>
2022-08-25 11:19:55 +0800

    【聊城】 交班报告10分钟不操作重新获取数据

--- src/store/module/shiftRecords.js
+++ src/store/module/shiftRecords.js
@@ -3,8 +3,11 @@ export default {
     state: {
       isLock: false,//是否有其他护士在编辑
       shiftRecordID:'',//当前锁定的交班报告
-      enterTime:''//进入的时间
+      enterTime:'', //进入的时间
+      setupTime:'' , //后台配置的默认解锁时间（min）
+      lockTimeId:'', //锁定重新加载页面
     },
+    // 懒得改了，传多个参数。写一个函数就可以
     mutations: {
       changeLockState(state,data) {
         state.isLock=data
@@ -14,7 +17,13 @@ export default {
       },
       changeEnterTime(state,data){
         state.enterTime=data
-      }
+      },
+      changeSetupTime(state,data){
+        state.setupTime=data
+      },
+      changeLockTimeId(state,data){
+        state.lockTimeId=data
+      },
     }
   }
   
