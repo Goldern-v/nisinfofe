f2e44300cd47be95b682c216f029d6ba14d66471
goldern <dingjunwei@dingjunweideMacBook-Pro.local>
2022-08-31 11:08:30 +0800

    [临邑]:病重（病危）患者护理记录单（新）入量名称容量修整

--- src/Page/sheet-page/components/config/critical_new_linyi/tr.js
+++ src/Page/sheet-page/components/config/critical_new_linyi/tr.js
@@ -186,7 +186,7 @@ export default [{
       top: "1px",
       bottom: "1px",
       left: "1px",
-      width: "360px",
+      width: "160px",
       background: "transparent",
       fontSize:"12px"
     },
