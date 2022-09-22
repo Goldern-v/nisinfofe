9581c0fc2ccf438da76f126e6b8839d17cc811d4
HeJinYin <328869170@qq.com>
2022-09-22 17:00:38 +0800

    【花都】  护记偶数页面打印不翻转

--- src/Part/print/component/sheet-print-page.vue
+++ src/Part/print/component/sheet-print-page.vue
@@ -292,8 +292,16 @@ export default {
       });
     }
     let sheetTableWidth = document.querySelector("div.contant").offsetWidth;
-    // 江门妇幼的护理单打印双数页面会翻转,横沥ICU机械通气护理单,武汉肺科。修复
-    if(this.HOSPITAL_ID==="sdlj" || this.HOSPITAL_ID==="fuyou"||this.sheetInfo.sheetType==="ventilation_hl"||this.HOSPITAL_ID==="wujing"||this.HOSPITAL_ID==="fsxt"||this.HOSPITAL_ID==="whfk"||this.HOSPITAL_ID==="foshanrenyi"||this.HOSPITAL_ID==="lyxrm"){
+    //   医院的护理单打印双数页面会翻转（双面打印）。这些医院都是不需要偶数页面打印翻转的
+    if(this.HOSPITAL_ID==="sdlj" 
+       || this.HOSPITAL_ID==="fuyou"
+       ||this.sheetInfo.sheetType==="ventilation_hl"
+       ||this.HOSPITAL_ID==="wujing"
+       ||this.HOSPITAL_ID==="fsxt"
+       ||this.HOSPITAL_ID==="whfk"
+       ||this.HOSPITAL_ID==="foshanrenyi"
+       ||this.HOSPITAL_ID==="lyxrm"
+       ||this.HOSPITAL_ID==="huadu"){
       printDir("h");
             addCSS(
               window,
