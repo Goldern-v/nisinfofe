db68204f93548878d0bee893f3529b7a83e6d6e3
小高 <8555921+gaohaixiong@user.noreply.gitee.com>
2022-10-19 10:42:33 +0800

    提交遗漏

--- src/api/lesion.js
+++ src/api/lesion.js
@@ -134,5 +134,6 @@ export {
   syncGetNurseBedRecSDLJExecute,
   syncGetNurseBedRecDGXGExecute,
   syncGetNurseBedRecBeiHaiExecute,
-  syncGetNursePatientWHFKRecData
+  syncGetNursePatientWHFKRecData,
+  syncGetNurseBedRecWHHKExecute
 }
