8fe67208fb3a902ef7f2c59970734596136a93fd
chentingqi <1051570119@qq.com>
2022-11-09 19:48:33 +0800

    佛一修复记录

--- src/Page/sheet-page/components/render/decode.js
+++ src/Page/sheet-page/components/render/decode.js
@@ -31,10 +31,11 @@ function decode(ayncVisitedData) {
       // 一定要做这个优化速度需求，被逼无奈，轻喷
       if (bodyModel.hasOwnProperty(index) && (!process.env.splitSave || bodyModel[index].isChange || (bodyModel[index][recordDateIndex] && bodyModel[index][recordDateIndex].value && changeArr.includes(bodyModel[index][recordDateIndex].value)))) {
         let tr = {};
+        const recordDate = bodyModel[index].find((item)=>item.key == "recordDate");
         for (let option of bodyModel[index]) {
           tr[option.key] = option.value;
-          if(bodyModel[index][0].value){
-          tr[`recordDate`] = bodyModel[index].find((item)=>item.key == "recordDate").value;
+          if(recordDate && recordDate.value) {
+            tr[`recordDate`] = recordDate.value
           }else{
             let itemRecordYear = bodyModel[index].find(
               item => item.key == "recordYear"
