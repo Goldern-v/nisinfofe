854add28a5ed3d5ce19a6f6edeb4ff4ec3bb0d01
hgy <1309843131@qq.com>
2022-11-09 10:07:00 +0800

    fix:珠海-患者详情会诊调整

--- src/Page/patientInfo/supPage/consultation/components/item/listItem.vue
+++ src/Page/patientInfo/supPage/consultation/components/item/listItem.vue
@@ -1,81 +1,147 @@
 <template>
   <div>
-    <div class="card_item" v-touch-ripple :class="{active: clickIndex == data.id}">
-					<span class="card_item-title">{{data.conGoal}}</span>
-					<div class="card_item-date">{{data.planDate}}</div>
-					<span class="card_item-tag" :class="{done: data.status == '完成', hold: data.status == '暂存', apply: data.status == '申请确认', wait: data.status == '等待确认'}">{{data.status}}</span>
-			</div>
+    <div
+		v-if="data.planDate != undefined"
+      class="card_item"
+      v-touch-ripple
+      :class="{ active: clickIndex == data.id }"
+    >
+      <span class="card_item-title">{{ data.conGoal }}</span>
+      <div class="card_item-date">
+        {{ data.planDate || data.consultationTime }}
+      </div>
+      <span
+        class="card_item-tag"
+        :class="{
+          done: data.status == '完成',
+          hold: data.status == '暂存',
+          apply: data.status == '申请确认',
+          wait: data.status == '等待确认',
+        }"
+        >{{ data.status }}</span
+      >
+    </div>
+		<div
+			v-else
+      class="card_item"
+      v-touch-ripple
+      :class="{ active: clickIndex == data.consultationId }"
+    >
+      <span class="card_item-title">{{ data.consultationName || '护理会诊单' }}</span>
+      <div class="card_item-date">{{ data.consultationTime }}</div>
+      <span class="card_item-tag" :class="filterClass(data.status)">{{
+        data.status | filterText
+      }}</span>
+    </div>
   </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
 .card_item {
-				font-size: 13px;
-				color: #687179;
-				padding 11px 14px 11px 14px
-        margin-bottom 10px
-        border: 1px solid #E4E8EB;
-        border-radius: 2px;  
-				box-sizing: border-box;
-				height: 65px;
-				position: relative;
-        cursor: pointer;
-				&.active{
-					background: #F2F2F2;
-				}
-				&-title {
-					font-size: 13px;
-					color: #687179;
-          overflow hidden;
-          white-space nowrap;
-          text-overflow ellipsis;
-          display block;
-          padding-right 80px;
-				}
-				&-date {
-					font-size: 12px;
-					color: #999999;
-					line-height: 36px;
-				}
-				&-tag {
-					font-size: 12px;
-					color: #fff;
-					border-radius: 2px;
-					padding: 2px 10px;
-					height: 18px;
-					line-height: 19px;
-					position: absolute;
-					right: 15px;
-					top: 10px;
-					&.done {
-						// 完成
-						background: #4BB08D;
-					}
-					&.hold {
-						// 暂存
-						background: #6CA8D7;
-					}
-					&.apply {
-						// 申请确认
-						background: #FD815A;
-					}
-					&.wait {
-						// 等待确认
-						background: #FD943A;
-					}
-				}
-			}
+  font-size: 13px;
+  color: #687179;
+  padding: 11px 14px 11px 14px;
+  margin-bottom: 10px;
+  border: 1px solid #E4E8EB;
+  border-radius: 2px;
+  box-sizing: border-box;
+  height: 65px;
+  position: relative;
+  cursor: pointer;
+
+  &.active {
+    background: #F2F2F2;
+  }
+
+  &-title {
+    font-size: 13px;
+    color: #687179;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    display: block;
+    padding-right: 80px;
+  }
+
+  &-date {
+    font-size: 12px;
+    color: #999999;
+    line-height: 36px;
+  }
+
+  &-tag {
+    font-size: 12px;
+    color: #fff;
+    border-radius: 2px;
+    padding: 2px 10px;
+    height: 18px;
+    line-height: 19px;
+    position: absolute;
+    right: 15px;
+    top: 10px;
+
+    &.done {
+      // 完成
+      background: #4BB08D;
+    }
+
+    &.hold {
+      // 暂存
+      background: #6CA8D7;
+    }
+
+    &.apply {
+      // 申请确认
+      background: #FD815A;
+    }
+
+    &.wait {
+      // 等待确认
+      background: #FD943A;
+    }
+
+		&.unaudited {
+      background: rgb(73, 164, 234);
+    }
+
+    &.audit {
+      background: rgb(239, 153, 77);
+    }
+
+    &.reject {
+      background: #ff4949;
+    }
+
+    &.cancel {
+      background: #687179;
+    }
+
+    &.archived {
+      background: rgb(94, 157, 70);
+    }
+  }
+}
 </style>
 <script>
-  export default {
-    props: {
-      data: Object,
-      clickIndex: Number
+import { STATUS_LIST } from "../../enums";
+
+export default {
+  props: {
+    data: Object,
+    clickIndex: [Number, String],
+  },
+  data() {
+    return {};
+  },
+	filters: {
+    filterText(status) {
+      return STATUS_LIST.find((v) => v.key == status).label || "";
     },
-    data() {
-      return {
-        msg: 'hello vue'
-      }
+  },
+  components: {},
+  methods: {
+    filterClass(status) {
+      return STATUS_LIST.find((v) => v.key == status).className || "";
     },
-    components: {}
-  }
+  },
+};
 </script>
--- src/Page/patientInfo/supPage/consultation/consultation.vue
+++ src/Page/patientInfo/supPage/consultation/consultation.vue
@@ -1,175 +1,203 @@
 <template>
-	<section class="consultation">
-		<newFormDialog ref="newFormDialog"></newFormDialog>
-		<section class="card" :style="{height: height}">
-			<div class="card_title">
-				<h4 class="card_title-desc">护理会诊记录</h4>
-				<button class="card_title-add" @click="$refs.newFormDialog.open()">
-						<i class="iconfont icon-guanbi"></i>新建会诊
-					</button>
-			</div>
-			<div class="card_wrap" :style="{height: scrollHeight}">
-				<div class="card_noitem" v-if="!list.length">
-					<img :src="require('@/common/images/task/nondata.png')" alt="" v-if="true">
-					<span>暂无护理会诊记录～</span>
-				</div>
-				<!-- 动态生成 -->
-				<span @click="toDetail(item)" v-for="item in list" :key="item.id"><listItem :data="item" :clickIndex="clickIndex"></listItem></span>
-			</div>
-		</section>
-		<section class="form" :style="{height: height}" v-show="clickIndex">
-			<consultationForm ref="consultationForm"></consultationForm>
-		</section>
-	</section>
+  <section class="consultation">
+    <component :is="newDialogCon" ref="newDialogConRef" />
+    <section class="card" :style="{ height: height }">
+      <div class="card_title">
+        <h4 class="card_title-desc">护理会诊记录</h4>
+        <button class="card_title-add" @click="$refs.newDialogConRef.open()">
+          <i class="iconfont icon-guanbi"></i>新建会诊
+        </button>
+      </div>
+      <div class="card_wrap" :style="{ height: scrollHeight }">
+        <div class="card_noitem" v-if="!list.length">
+          <img :src="require('@/common/images/task/nondata.png')" alt="" />
+          <span>暂无护理会诊记录～</span>
+        </div>
+        <!-- 动态生成 -->
+        <span @click="toDetail(item)" v-for="item in list" :key="item.id">
+          <listItem :data="item" :clickIndex="clickIndex" />
+        </span>
+      </div>
+    </section>
+    <section class="form" :style="{ height: height }" v-show="clickIndex">
+      <component :is="detailCon" ref="consultationForm"></component>
+    </section>
+  </section>
 </template>
 
 
 <script>
-	import newFormDialog from './components/newFormDialog'
-	import consultationForm from './components/consultationForm'
-	import {
-		getPatientList
-	} from '@/api/consultation'
-	import listItem from './components/item/listItem.vue'
-	import BusFactory from 'vue-happy-bus'
-	export default {
-		data() {
-			return {
-				formVisible: false,
-				list: [],
-				selectItem: {},
-				clickIndex: 0,
-				bus: BusFactory(this)
-			}
-		},
-		computed: {
-			wih() {
-				return this.$store.state.common.wih
-			},
-			height() {
-				return `${this.wih - 150}px`
-			},
-			scrollHeight() {
-				return `${this.wih - 186}px`
-			}
-		},
-		mounted() {},
-		created() {
-			this.getData().then((res) => {
-				if (res[0]) {
-					this.toDetail(res[0])
-				}
-			})
-			// 刷新会诊列表
-			this.bus.$on('consultationListRefresh', () => {
-				this.getData()
-			})
-		},
-		methods: {
-			toDetail(item) {
-				this.clickIndex = item.id
-				this.$refs.consultationForm.open(item)
-			},
-			getData() {
-				return getPatientList(this.$route.query.patientId, this.$route.query.visitId).then(res => {
-					this.list = res.data.data
-					return this.list
-				})
-			},
-			aaa(item) {
-				this.bus.$emit('aaa')
-			},
-		},
-		components: {
-			newFormDialog,
-			consultationForm,
-			listItem
+import newFormDialog from "./components/newFormDialog";
+import consultationForm from "./components/consultationForm";
+import consultationFormZhzxy from "./components/consultationFormZhzxy";
+import { getPatientList, getPatientListNew } from "@/api/consultation";
+import listItem from "./components/item/listItem.vue";
+import BusFactory from "vue-happy-bus";
+import newFormDialogZhzxy from "./components/newFormDialogZhzxy.vue";
+export default {
+  data() {
+    return {
+      formVisible: false,
+      list: [],
+      selectItem: {},
+      clickIndex: 0,
+      bus: BusFactory(this),
+      // 是否新版会诊
+      isNewType: ["zhzxy"].includes(this.HOSPITAL_ID),
+    };
+  },
+  computed: {
+    wih() {
+      return this.$store.state.common.wih;
+    },
+    height() {
+      return `${this.wih - 150}px`;
+    },
+    scrollHeight() {
+      return `${this.wih - 186}px`;
+    },
+		// 新增弹窗
+    newDialogCon() {
+      if (this.isNewType) {
+        return "newFormDialogZhzxy";
+      } else {
+        return "newFormDialog";
+      }
+    },
+		// 详情
+		detailCon() {
+      if (this.isNewType) {
+        return "consultationFormZhzxy";
+      } else {
+        return "consultationForm";
+      }
 		}
-	}
+  },
+  mounted() {
+    this.getData().then((res) => {
+      if (res[0]) {
+        this.toDetail(res[0]);
+      }
+    });
+    // 刷新会诊列表
+    this.bus.$on("consultationListRefresh", () => {
+      this.getData();
+    });
+  },
+  methods: {
+    toDetail(item) {
+      this.clickIndex = item.id || item.consultationId || '';
+      this.clickIndex && this.$refs.consultationForm.open(item);
+    },
+    getData() {
+      if (this.isNewType) {
+        return getPatientListNew(this.$route.query.patientId).then((res) => {
+          this.list = res.data.data || [];
+          return this.list;
+        });
+      } else {
+        return getPatientList(
+          this.$route.query.patientId,
+          this.$route.query.visitId
+        ).then((res) => {
+          this.list = res.data.data;
+          return this.list;
+        });
+      }
+    },
+  },
+  components: {
+    newFormDialog,
+    newFormDialogZhzxy,
+    consultationForm,
+    consultationFormZhzxy,
+    listItem,
+  },
+};
 </script>
 
 
 <style lang="scss" scoped>
-	.consultation {
-		margin: 20px 20px; // display: flex;
-		// justify-content: space-between;
-	}
-	.card {
-		background: #fff;
-		border: 1px solid #cbd5dd;
-		border-radius: 2px; // height: 300px;
-		width: 36%;
-		float: left;
-		&_title {
-			overflow: hidden;
-			background: #f7fafa;
-			height: 35px;
-			padding: 0 4%;
-			color: #333;
-			border-bottom: 1px solid #EAEEF1;
-			&-desc {
-				display: inline;
-				font-family: 'PingFangSC-Medium';
-				font-size: 13px;
-				font-weight: bold;
-				color: #333;
-				letter-spacing: 0;
-				line-height: 35px;
-			}
-			&-add {
-				float: right;
-				border: none;
-				background: transparent;
-				outline: none;
-				cursor: pointer;
-				font-size: 1px;
-				color: #687179;
-				i {
-					display: inline-block;
-					font-size: 12px;
-					line-height: 35px;
-					transform: rotate(45deg);
-					margin-right: 4px;
-				}
-			}
-		}
-		&_wrap {
-			padding: 20px;
-			overflow-y: auto;
-			box-sizing: border-box;
-			position: relative;
-			.card_noitem {
-				width: 100%;
-				height: 100%;
-				img,
-				span {
-					position: absolute;
-					left: 50%;
-					transform: translate(-50%, 0);
-				}
-				img {
-					width: 140px;
-					height: 140px;
-					top: 180px;
-				}
-				span {
-					top: 340px;
-					display: inline-block;
-					font-size: 13px;
-					color: #CBD5DD;
-				}
-			}
-		}
-	}
-	.form {
-		float: right;
-		width: 63%;
-		background: #F7FAFA;
-		border: 1px solid #CBD5DD;
-		border-radius: 2px;
-		padding: 20px;
-		overflow: auto;
-		box-sizing: border-box;
-		position: relative;
-	}
+.consultation {
+  margin: 20px 20px; // display: flex;
+  // justify-content: space-between;
+}
+.card {
+  background: #fff;
+  border: 1px solid #cbd5dd;
+  border-radius: 2px; // height: 300px;
+  width: 36%;
+  float: left;
+  &_title {
+    overflow: hidden;
+    background: #f7fafa;
+    height: 35px;
+    padding: 0 4%;
+    color: #333;
+    border-bottom: 1px solid #eaeef1;
+    &-desc {
+      display: inline;
+      font-family: "PingFangSC-Medium";
+      font-size: 13px;
+      font-weight: bold;
+      color: #333;
+      letter-spacing: 0;
+      line-height: 35px;
+    }
+    &-add {
+      float: right;
+      border: none;
+      background: transparent;
+      outline: none;
+      cursor: pointer;
+      font-size: 1px;
+      color: #687179;
+      i {
+        display: inline-block;
+        font-size: 12px;
+        line-height: 35px;
+        transform: rotate(45deg);
+        margin-right: 4px;
+      }
+    }
+  }
+  &_wrap {
+    padding: 20px;
+    overflow-y: auto;
+    box-sizing: border-box;
+    position: relative;
+    .card_noitem {
+      width: 100%;
+      height: 100%;
+      img,
+      span {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, 0);
+      }
+      img {
+        width: 140px;
+        height: 140px;
+        top: 180px;
+      }
+      span {
+        top: 340px;
+        display: inline-block;
+        font-size: 13px;
+        color: #cbd5dd;
+      }
+    }
+  }
+}
+.form {
+  float: right;
+  width: 63%;
+  background: #f7fafa;
+  border: 1px solid #cbd5dd;
+  border-radius: 2px;
+  padding: 20px;
+  overflow: auto;
+  box-sizing: border-box;
+  position: relative;
+}
 </style>
