f9b9de1399c702a9707c11e6a71931644e771d1f
Y-zone <8555921+gaohaixiong@user.noreply.gitee.com>
2022-11-23 17:41:32 +0800

    武汉汉口新增会诊模块

--- src/Page/patientInfo/supComponts/topPart_Whhk.vue
+++ src/Page/patientInfo/supComponts/topPart_Whhk.vue
@@ -182,6 +182,9 @@
       >
         <div class="nav-item">体温单</div>
       </router-link>
+       <router-link :to="{path:'/consultation', query:$route.query}" tag="span">
+        <div class="nav-item">会诊</div>
+      </router-link>
       <!-- <router-link
         :to="{path:'/otherPage', query: {patientId:query.patientId, visitId: query.visitId}}"
         tag="span"
