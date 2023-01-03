8f7e94f0d5852500260ae53392d3fe2e4702ea7e
HeJinYin <328869170@qq.com>
2023-01-03 16:21:42 +0800

    【寮步】优化表单

--- src/Page/sheet-page/components/config/prenatal_dglb/tr.js
+++ src/Page/sheet-page/components/config/prenatal_dglb/tr.js
@@ -14,19 +14,6 @@ import {
   click_date,
   click_time
 } from "../keyEvent/date";
-import info from "../sheetInfo";
-
-const zgssList = ['软', '硬', '中'];
-const gdgdList = ['U+1', 'U+2', 'U+3', 'U=0', 'U-1', 'U-2', 'U-3', 'U-4'];
-const rfqkList = ['(-)', '硬'];
-const mrqkList = ['少', '中等', '多'];
-const xtlist = ['+', '-']
-const skqkList = ['红肿', '渗液', '水肿', '(-)', '会阴(-)'];
-const 意识 = ['清醒', '浅昏迷', '深昏迷', '模糊', '嗜睡', '昏睡', '谵妄'];
-const zdjnList = ['✓'];
-const 入量名称 = ['输液', '口服', '输血', '肌注', '静注', '鼻饲', '静脉泵入', '灌肠', '饮水'];
-const 出量颜色 = ['黄色', '鲜红色', '暗红色', '墨绿色', '浓茶色','淡红色','茶褐色'];
-const 出量名称 = ['呕吐物', '伤口引流', '腋窝引流', '胸壁引流', '胸腔引流', '腹腔引流', '心包积液', '胸腔引流液', '大便', '尿', '腹水', '脑积液', '痰液', '血液', '胃管', '尿管', '盆腔引流管', '肝下引流管','温氏孔引流管', '脾窝引流管', '伤口皮下引流管', 'T管', '隔下引流管', '人工肛', '胃造痰', '空肠造痰', '肛管引流管','杜氏窝引流管', '左肾造痰', '右肾造痰', '膀胱造痰', '左肾周引流', '右肾周引流', '耻骨后引流', '左输尿管导管引流', '右输尿管导管引流', '左肾窝引流', '右肾窝引流', '会阴部引流', '左输尿管造口支架管', '右输尿管造口支架管', '阴道出血', '术中出血', '止血球囊引流', '腹透液', '分娩时出血'];
 
 export default [
   {
@@ -131,112 +118,6 @@ export default [
       width: 43
     },
   },
-  // {
-  //   key: "fieldThree", //阴道出血mL
-  //   value: "",
-  //   event: keyf1,
-  //   name: "阴道出血",
-  //   next: "mL",
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 35
-  //   },
-  // },
-  // {
-  //   key: "fieldSix", //子宫收缩
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 30
-  //   },
-  //   autoComplete: {
-  //     data: zgssList
-  //   },
-  // },
-  // {
-  //   key: "fieldSeven", //宫底高度
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 30
-  //   },
-  //   autoComplete: {
-  //     data: gdgdList
-  //   },
-  // },
-  // {
-  //   key: "fieldEight", //乳房情况
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 30
-  //   },
-  //   autoComplete: {
-  //     data: rfqkList
-  //   },
-  // },
-  // {
-  //   key: "fieldNine", //泌乳情况
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 30
-  //   },
-  //   autoComplete: {
-  //     data: mrqkList
-  //   },
-  // },
-
-  // {
-  //   key: "fieldEleven", //指导挤奶
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 30
-  //   },
-  //   autoComplete: {
-  //     data: zdjnList
-  //   },
-  // },
-  // {
-  //   key: "dressing", //腹部切口敷料
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 48
-  //   },
-  //   autoComplete: {
-  //     data: ['干洁', '渗液']
-  //   },
-  // },
-  // {
-  //   key: "perineal", //会阴伤口
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 48
-  //   },
-  //   autoComplete: {
-  //     data: ['（—）', '红肿', '水肿']
-  //   },
-  // },
-  // {
-  //   key: "exhaust", //肛门排气/肠鸣音<br /> (次/分)
-  //   value: "",
-  //   event: keyf1,
-  //   change: (e, td) => limitChange(e, td, 4),
-  //   textarea: {
-  //     width: 48
-  //   },
-
-  // },
   {
     key: "food", // 入量内容
     value: "",
@@ -313,6 +194,9 @@ export default [
     event: keyf1,
     name: "胎方位",
     next: "",
+    autoComplete: {
+      data: ['LOA','LOT','LOP','ROA','ROT','ROP','LMA','RMA','LSA','RSA','LScA','RScA','LScp','RScp','横位']
+    },
     change: (e, td) => limitChange(e, td, 4),
     textarea: {
       width: 43
@@ -347,6 +231,9 @@ export default [
     event: keyf1,
     name: "强度",
     next: "",
+    autoComplete: {
+      data: ['无','偶有','不规则','弱','中','强']
+    },
     change: (e, td) => limitChange(e, td, 4),
     textarea: {
       width: 43
@@ -413,6 +300,9 @@ export default [
     event: keyf1,
     name: "胎膜",
     next: "",
+    autoComplete: {
+      data: ['已破','未破']
+    },
     change: (e, td) => limitChange(e, td, 4),
     textarea: {
       width: 43
@@ -457,6 +347,9 @@ export default [
     event: keyf1,
     name: "膝反射",
     next: "",
+    autoComplete: {
+      data: ['+','-']
+    },
     change: (e, td) => limitChange(e, td, 4),
     textarea: {
       width: 43
