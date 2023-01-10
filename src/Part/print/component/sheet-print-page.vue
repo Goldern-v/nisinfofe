1bdf3e686c4c368dea89c52396936a971b5c88b1
xCnccw <1093933436@qq.com>
2023-01-10 15:25:49 +0800

    [bug修改]

--- src/Part/print/component/sheet-print-page.vue
+++ src/Part/print/component/sheet-print-page.vue
@@ -72,12 +72,8 @@
     width: 90px !important;
   }
   th[dataname='签名'] {
-    width: 60px !important;
+    width: 100px !important;
   }
-  th[dataname='签 名'] {
-    width: 60px !important;
-  }
-
   th[dataname='审核签名'] {
     display: none !important;
   }
@@ -768,13 +764,27 @@ export default {
     }
     // 如果双签可以这里加。打印的时候签名二合一。签名列拉宽
     if (
-      (sheetInfo.sheetType == "generalnursing_tj"||sheetInfo.sheetType == "baby_tj")
+      (sheetInfo.sheetType == "generalnursing_tj")
     ) {
       addCSS(
         
         window,
         `
             #sheetPagePrint th[dataname="护士<br/>签名"] {
+              min-width: 100px !important;
+              max-width: 100px !important;
+           }
+        `
+      );
+    }
+    if (
+      (sheetInfo.sheetType == "baby_tj")
+    ) {
+      addCSS(
+        
+        window,
+        `
+            #sheetPagePrint th[dataname="签 名"] {
               min-width: 100px !important;
               max-width: 100px !important;
            }
