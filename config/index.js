6229c24d622abda3948436fdec3ad01ce4637b88
xCnccw <1093933436@qq.com>
2023-02-23 14:57:27 +0800

    佛一 麻醉科AICU护理记录单 功能修改

--- config/index.js
+++ config/index.js
@@ -160,11 +160,12 @@ const currentTargetUrl = (() => {
      case 'foshanrenyi':
       // return "https://cr.fsyyy.com:9094"  // 正式外网地址
       // return "http://192.168.20.147:9091"  // 双艳
-      return "http://218.204.204.90:9095" // 默认公司本地内网厚街
+      // return "http://218.204.204.90:9095" // 默认公司本地内网厚街
       // return "http://192.168.20.102:8080" //宪锋本地地址
       // return "http://120.24.240.231:19097" //
       // return "http://192.168.103.16:9091" //
       // return "http://218.13.87.27:9094" //
+      return "http://192.168.1.54:9883" //测试
       // return "https://info.cr-health.com:20215"
 
     // 福清妇幼保健院
