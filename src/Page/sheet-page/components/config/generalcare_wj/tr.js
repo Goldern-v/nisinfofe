8c195f54026fc3d7119812e5c7d65fc7c727f2eb
veagxxx <1608237085@qq.com>
2023-02-28 16:24:36 +0800

    fix：北海护记下拉项

---
+++ src/Page/sheet-page/components/config/generalcare_wj/tr.js
@@ -0,0 +1,139 @@
+/*
+北海人一-一般患者护理记录
+*/
+import {
+  multiDictInfo
+} from "../../../api/index";
+import {
+  keyf1,
+  limitChange
+} from "../keyEvent/f1.js";
+import {
+  event_date,
+  event_time,
+  click_date,
+  click_time
+} from "../keyEvent/date";
+
+export default [{
+    key: "recordMonth", //日期
+    value: "",
+    event: event_date,
+    click: click_date
+  },
+  {
+    key: "recordHour", //时间
+    value: "",
+    event: event_time,
+    click: click_time
+  },
+  {
+    key: "description", //特殊情况记录
+    value: "",
+    style: {
+      textAlign: "left",
+      position: "absolute",
+      top: "1px",
+      bottom: "1px",
+      left: "1px",
+      width: "810px",
+      background: "transparent",
+    },
+    change: (e, td) => limitChange(e, td, 66),
+    textarea: {
+      width: 810
+    },
+    event: function (e, td) {
+      console.log(e.keyCode);
+      if (e.keyCode == 9) {
+        td.value = "    " + td.value;
+        e.preventDefault();
+      }
+      keyf1(e, td);
+    }
+  },
+  {
+    key: "sign",
+    value: ""
+  },
+  // {
+  //   key: "audit",
+  //   value: ""
+  // },
+  {
+    hidden: true,
+    key: "id",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "signerName",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "signerName2",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "status",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "recordSource",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "recordYear",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "dataHash",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "recordDate",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "monthHour",
+    value: ""
+  },
+  {
+    hidden: false,
+    key: "signerNo",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "signerNo2",
+    value: ""
+  },
+  {
+    hidden: false,
+    key: "auditorNo",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "auditorName",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "empNo",
+    value: ""
+  },
+  {
+    hidden: true,
+    key: "multiSign",
+    value: true,
+  },
+];
+
