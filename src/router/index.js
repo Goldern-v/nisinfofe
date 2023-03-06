aaecb62d1b980932dd2c074c151a9557ed49fd7e
veagxxx <1608237085@qq.com>
2023-03-06 11:03:31 +0800

    fix：威海导管修改

--- src/router/index.js
+++ src/router/index.js
@@ -417,11 +417,11 @@ const statisticalBloodPressure = () => import("@/Page/statistical-query/statisti
 const statisticalTemperature = () => import("@/Page/statistical-query/statistical-temperature/index.vue")
 const statisticalNursingLv = () => import("@/Page/statistical-query/statistical-nursingLv/index.vue")
 const statisticalWorkload = () => import("@/Page/statistical-query/statistical-workload/index.vue")
-const statisticalWorkloadZhzxy = () => import("@/Page/statistical-query/statistical-workload_zhzxy/index.vue")
+// const statisticalWorkloadZhzxy = () => import("@/Page/statistical-query/statistical-workload_zhzxy/index.vue")
 const statisticalBreath = () => import("@/Page/statistical-query/statistical-breath/index.vue")
 const statisticalExitAdmission = () => import("@/Page/statistical-query/statistical-exitAdmission/index.vue")
 const statisticalVTE = () => import("@/Page/statistical-query/statistical-VTE/index.vue")
-const StatisticalConsultation = () => import("@/Page/statistical-query/statistical-consultation/index.vue")
+// const StatisticalConsultation = () => import("@/Page/statistical-query/statistical-consultation/index.vue")
 const testGuizhou = () => import("@/Page/patientInfo/supPage/test/testGuizhou")
 Vue.use(Router);
 const HOSPITAL_ID = process.env.HOSPITAL_ID;
@@ -1425,7 +1425,7 @@ const router = new Router({
             component: sheetHospitalAdmissionPage
           }
         ]
-        
+
       },
       {
         path: "/sheetHospitalEval",
@@ -1809,8 +1809,8 @@ const router = new Router({
             },
             component: (() => {
               switch (HOSPITAL_ID) {
-                case 'zhzxy':
-                  return statisticalWorkloadZhzxy
+                // case 'zhzxy':
+                //   return statisticalWorkloadZhzxy
                 default:
                   return statisticalWorkload;
               }
@@ -1840,14 +1840,14 @@ const router = new Router({
             },
             component: statisticalVTE,
           },
-          {
-            path: "statisticalConsultation",
-            name: "statisticalConsultation",
-            meta: {
-              title: '会诊统计'
-            },
-            component: StatisticalConsultation,
-          },
+          // {
+          //   path: "statisticalConsultation",
+          //   name: "statisticalConsultation",
+          //   meta: {
+          //     title: '会诊统计'
+          //   },
+          //   component: StatisticalConsultation,
+          // },
         ]
       },
     ]
