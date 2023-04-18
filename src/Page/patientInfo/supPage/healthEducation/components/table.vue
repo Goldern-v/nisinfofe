e97f6440273016f4edadab2aa63860c23f7e45ae
else <15102082382@163.com>
2023-04-18 16:58:10 +0800

    fix：青海五院 -18823 【功能优化】PDA 推送健康宣教优化

--- src/Page/patientInfo/supPage/healthEducation/components/table.vue
+++ src/Page/patientInfo/supPage/healthEducation/components/table.vue
@@ -55,9 +55,9 @@
             <span class="remark-span">{{data[`${HOSPITAL_ID === 'whhk' ? '宣教情况' : '备注'}`]}}</span>
           </td>
           <!-- 签名 -->
-          <td v-if="['lingcheng','guizhou','foshanrenyi'].includes(HOSPITAL_ID)" class="specialTd">
+          <td v-if="['lingcheng','guizhou','foshanrenyi','qhwy'].includes(HOSPITAL_ID)" class="specialTd">
             <img
-              v-if="HOSPITAL_ID=='lingcheng' || HOSPITAL_ID=='foshanrenyi'"
+              v-if="['lingcheng','foshanrenyi','qhwy'].includes(HOSPITAL_ID)"
               v-show="data['lc签名']"
               class="img"
               :src="`/crNursing/api/file/signImage/${data['lc签名']}?${token}`"
