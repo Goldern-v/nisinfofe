3413674661b2a2bf051d4570cca1a11cf3119d26
goldern <1585904165@qq.com>
2023-05-22 09:11:26 +0800

    增加项目过期功能

--- src/Page/admissionHisView/admissionHisView.vue
+++ src/Page/admissionHisView/admissionHisView.vue
@@ -17,7 +17,23 @@ return {};
 methods: {
     ...mapMutations('common', [
         'setUser'
-    ])
+    ]),
+    todo(res,next){
+        let user = res.data.data.user;
+        user.token = res.data.data.authToken;
+        window.app.authToken = res.data.data.authToken;
+        localStorage.setItem("user",JSON.stringify(res.data.data.user))
+        localStorage.setItem("sso",'sso')
+        this.setUser(res.data.data.user || {})
+        Cookies.set(
+            "NURSING_USER",
+            `${res.data.data.user.id}##${res.data.data.authToken}`,
+            {
+            path: "/",
+            }
+        );
+        next && next()
+    }
 },
 created(){
 },
@@ -29,21 +45,32 @@ beforeRouteEnter(to,from,next){
         let {userName} = to.query
         autoLogin({empNo:userName,token:''}).then(res=>{
             // console.log(res.data);
-            if(res.status==200&&res.data.code==200){
-                let user = res.data.data.user;
-                user.token = res.data.data.authToken;
-                window.app.authToken = res.data.data.authToken;
-                localStorage.setItem("user",JSON.stringify(res.data.data.user))
-                localStorage.setItem("sso",'sso')
-                this.setUser(res.data.data.user || {})
-                Cookies.set(
-                    "NURSING_USER",
-                    `${res.data.data.user.id}##${res.data.data.authToken}`,
-                    {
-                    path: "/",
-                    }
-                );
-                next()
+            if(res.data.code =='402'){
+            this.$confirm(resd.data.data.expireDesc, "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning",
+            })
+            .then(() => {
+            this.todo(res,next)
+            })
+            .catch(()=>{
+            this.todo(res,next)
+            })
+        }else if(res.data.code==='403'){
+            this.$confirm(res.data.desc, "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning",
+            })
+            .then(() => {
+            this.$router.go(-1)
+            })
+            .catch(()=>{
+            this.$router.go(-1)
+            })
+        }else if(res.status==200&&res.data.code==200){
+            this.todo(res,next)
             }
         })
     }
