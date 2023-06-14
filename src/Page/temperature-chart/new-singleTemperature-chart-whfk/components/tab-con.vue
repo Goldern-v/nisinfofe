ed77648e73252aac583da8369a8ac4edea25cb1c
else <15102082382@163.com>
2023-06-14 14:45:21 +0800

    fix：武汉肺科--20749--体温单脉搏范围修改

--- src/Page/temperature-chart/new-singleTemperature-chart-whfk/components/tab-con.vue
+++ src/Page/temperature-chart/new-singleTemperature-chart-whfk/components/tab-con.vue
@@ -757,7 +757,7 @@ export default {
           let y = {
             脉搏: {
               value: val,
-              reg: [20, 180],
+              reg: [20, 300],
             },
           };
           return y;
