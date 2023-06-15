497e4b2421f2642652c045bcc4b16d2dd65cb489
veagxxx <1608237085@qq.com>
2023-06-15 17:45:55 +0800

    fix：parse5包导致打包报错

--- build-new/webpack.base.conf.js
+++ build-new/webpack.base.conf.js
@@ -72,7 +72,8 @@ module.exports = {
           // resolve("node_modules/.4.1.2@view-design"),
           resolve("node_modules/_view-design@4.0.2@view-design"),
           resolve("node_modules/crypto-js"),
-          resolve("node_modules/loader-utils")
+          resolve("node_modules/loader-utils"),
+          resolve("node_modules/parse5"),
         ],
         options: { plugins: ['transform-class-properties'] }
         // options: {
