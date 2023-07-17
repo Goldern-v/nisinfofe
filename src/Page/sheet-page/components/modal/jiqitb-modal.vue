031c897941dd1c951519509b117252eae0d3e1f0
goldern <1585904165@qq.com>
2023-07-17 09:20:37 +0800

    🐯 监护仪同步时间更改

--- src/Page/sheet-page/components/modal/jiqitb-modal.vue
+++ src/Page/sheet-page/components/modal/jiqitb-modal.vue
@@ -118,7 +118,7 @@ export default {
     },
     open(type) {
       let startDate = moment().format("yyyy-MM-DD") + " 00:00:00";
-      let endDate = moment().format("yyyy-MM-DD HH:mm:ss");
+      let endDate = moment().format("yyyy-MM-DD"+ " 23:59:59");
       this.longDate = [startDate, endDate];
       if (type === "jianhuyi") {
         this.coloums = jianhuColoums;
