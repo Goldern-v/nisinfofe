d5c8114deaf9d24c548a19029429407418f936f9
Y-zone <2608347103@qq.com>
2023-08-18 15:30:46 +0800

    🎈 perf:【江门妇幼】ca签名 22993

--- src/components/modal/sign.vue
+++ src/components/modal/sign.vue
@@ -48,7 +48,7 @@
           :disabled="['nanfangzhongxiyi' ].includes(HOSPITAL_ID) && nanfangCa"
           placeholder="输入用户名或者工号"
           v-model="username"
-          :readonly="['foshanrenyi','weixian','zzwy'].includes(HOSPITAL_ID)"
+          :readonly="['foshanrenyi','weixian', 'fuyou'].includes(HOSPITAL_ID)"
         ></el-input>
       </div>
     </span>
@@ -79,7 +79,7 @@
         </div>
       </div>
     </span>
-    <span v-else-if="(!['foshanrenyi','weixian'].includes(HOSPITAL_ID)) || pw ">
+    <span v-else-if="(!['foshanrenyi','weixian', 'fuyou'].includes(HOSPITAL_ID)) || pw ">
       <p for class="name-title">{{ label }}</p>
       <div ref="passwordInput">
         <el-input
@@ -102,7 +102,7 @@
       </p>
     </span>
 
-    <div v-if="['foshanrenyi','weixian'].includes(HOSPITAL_ID)" style="margin-top: 5px">
+    <div v-if="['foshanrenyi','weixian', 'fuyou'].includes(HOSPITAL_ID)" style="margin-top: 5px">
       <span @click="openCaSignModal" class="loginCa" v-if="!ca_isLogin"
         >登录证书</span
       >
@@ -378,7 +378,7 @@ export default {
         if([].includes(this.HOSPITAL_ID) && !this.nanfangCa){
           this.pw = true
         }else this.pw = false;
-      
+
       // 如果已经是ca签名的情况下 点开签名弹窗 默认开启ca签名
       if(['hj'].includes(this.HOSPITAL_ID)){
         if(this.fuyouCaData && this.fuyouCaData.userName) {
@@ -393,7 +393,7 @@ export default {
       this.password = "";
 
       this.$refs.modalName.open();
-      if (!['foshanrenyi','weixian'].includes(this.HOSPITAL_ID)) {
+      if (!['foshanrenyi','weixian', 'fuyou'].includes(this.HOSPITAL_ID)) {
         this.$nextTick(() => {
           if(showDate){
             let dateInput = this.$refs.dateInput.querySelector("input");
@@ -434,7 +434,7 @@ export default {
     post() {
       this.btnLoading = true
       this.setCloseCallback(null);
-      if (['foshanrenyi','weixian'].includes(this.HOSPITAL_ID)) {
+      if (['foshanrenyi','weixian', 'fuyou'].includes(this.HOSPITAL_ID)) {
         if (this.pw) {
           if (this.password == "") {
             return this.$message({
