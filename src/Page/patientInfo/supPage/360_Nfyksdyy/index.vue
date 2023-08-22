82d56b872138563f7a269ce3c0da13ef4773d709
pgm <1796621138@qq.com>
2023-08-22 10:24:48 +0800

    fix: 南方医科大学顺德医院-项目内嵌360视图页面

---
+++ src/Page/patientInfo/supPage/360_Nfyksdyy/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <iframe :src="url" style="width: 100%; height: calc(100vh - 53px);"></iframe>
+  </div>
+</template>
+
+<script>
+import common from "@/common/mixin/common.mixin";
+
+export default {
+  mixins: [common],
+  data() {
+    return {
+      url: ''
+    };
+  },
+  computed: {
+  },
+  mounted() {
+    let { inpNo } = this.$route.query
+    let cutInpNo = inpNo && inpNo.split('_')[0]
+    this.url = `http://192.168.5.74:8000/cdr-c/cdrView.do?type=102&attribute=${cutInpNo}&emplCode=${this.empNo}&authCode=df10814b-fa20-4978-5782-587c6f54f8e7&module=`
+  }
+};
+</script>
