77f597ac27415a88c7d862beeb7f478a8e20da6f
Y-zone <2608347103@qq.com>
2023-08-28 14:28:08 +0800

    🐞 fix:【杏坛】记录单诊断问题

--- src/Page/sheet-page/components/sheetTable/components/table-head/table-head-foshanxingtan.vue
+++ src/Page/sheet-page/components/sheetTable/components/table-head/table-head-foshanxingtan.vue
@@ -384,8 +384,21 @@ export default {
     }
   },
     diagnosis() {
+      /** 最接近的index */
+      let realIndex = 0;
+      let keys = Object.keys(this.sheetInfo.relObj || {});
+      for (let i = 0; i < keys.length; i++) {
+        let [base, keyIndex] = keys[i].split("PageIndex_diagnosis_");
+        if (keyIndex !== undefined) {
+          if (this.index >= keyIndex) {
+            if (this.index - keyIndex <= this.index - realIndex) {
+              realIndex = keyIndex;
+            }
+          }
+        }
+      }
       return (
-        (this.sheetInfo.relObj || {})[`PageIndex_diagnosis_${this.index}`] ||
+        (this.sheetInfo.relObj || {})[`PageIndex_diagnosis_${realIndex}`] ||
         this.patientInfo.diagnosis
       );
     },
@@ -503,7 +516,7 @@ export default {
     updateDiagnosis(key, label, autoText) {
       window.openSetTextModal(
         (text) => {
-          sheetInfo.relObj[`PageIndex_diagnosis_${this.index}`] = text;
+          this.sheetInfo.relObj[`PageIndex_diagnosis_${this.index}`] = text;
           this.$message.success(`修改诊断成功`);
           this.bus.$emit("saveSheetPage", false);
         },
