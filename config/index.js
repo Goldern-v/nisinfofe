b9b54524daa4ffff8df4585da82d6a6c2537545e
Y-zone <2608347103@qq.com>
2023-09-28 15:38:45 +0800

    🎈 perf:【武汉汉口】交班志点开患者病历没有刷新患者数据

--- config/index.js
+++ config/index.js
@@ -80,9 +80,9 @@ const currentTargetUrl = (() => {
 
     /** 聊城 */
     case "liaocheng":
-      return "http://120.224.211.7:61026" //聊城正式
+      // return "http://120.224.211.7:61026" //聊城正式
       // return "https://info.cr-health.com:20209" // 公司-聊城测试-外网
-      // return "http://192.168.1.54:9871" //公司-聊城测试-内网
+      return "http://192.168.1.54:9871" //公司-聊城测试-内网
       // return "http://192.168.2.122:8080" // 初升本地
       // return "http://192.168.20.83:8080"  //伟彬本地
 
